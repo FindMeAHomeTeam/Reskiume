0c8dcaf45a79b40e169f913fdef4e0047e9c78f8
juancoob <ja.cobos.obrero@gmail.com>
2025-10-07 13:19:20 +0200

    feat: Add CreateUserWithEmailAndPassword & update injections

--- composeApp/src/commonMain/kotlin/com/findmeahometeam/reskiume/di/AuthModule.kt
+++ composeApp/src/commonMain/kotlin/com/findmeahometeam/reskiume/di/AuthModule.kt
@@ -1,11 +1,18 @@
 package com.findmeahometeam.reskiume.di
 
 import com.findmeahometeam.reskiume.domain.usecases.ObserveAuthState
-import com.findmeahometeam.reskiume.domain.usecases.SignInWithEmail
+import com.findmeahometeam.reskiume.domain.usecases.CreateUserWithEmailAndPassword
+import com.findmeahometeam.reskiume.domain.usecases.SignInWithEmailAndPassword
+import com.findmeahometeam.reskiume.ui.profile.createAccount.CreateAccountViewmodel
+import com.findmeahometeam.reskiume.ui.profile.login.LoginViewmodel
 import org.koin.core.module.dsl.factoryOf
+import org.koin.core.module.dsl.viewModelOf
 import org.koin.dsl.module
 
 val authModule = module {
     factoryOf(::ObserveAuthState)
-    factoryOf(::SignInWithEmail)
+    factoryOf(::CreateUserWithEmailAndPassword)
+    factoryOf(::SignInWithEmailAndPassword)
+    viewModelOf(::CreateAccountViewmodel)
+    viewModelOf(::LoginViewmodel)
 }
---
+++ composeApp/src/commonMain/kotlin/com/findmeahometeam/reskiume/domain/usecases/CreateUserWithEmailAndPassword.kt
@@ -0,0 +1,11 @@
+package com.findmeahometeam.reskiume.domain.usecases
+
+import com.findmeahometeam.reskiume.data.remote.response.AuthResult
+import com.findmeahometeam.reskiume.domain.repository.AuthRepository
+
+class CreateUserWithEmailAndPassword(private val repository: AuthRepository) {
+
+    suspend operator fun invoke(email: String, password: String): AuthResult =
+        repository.createUserWithEmailAndPassword(email, password)
+
+}

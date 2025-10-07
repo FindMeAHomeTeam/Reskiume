2922e29eb6c036ade0c1037882331dd6db309b7c
juancoob <ja.cobos.obrero@gmail.com>
2025-10-07 13:13:23 +0200

    chore: Update SignInWithEmailAndPassword use case name

--- composeApp/src/commonMain/kotlin/com/findmeahometeam/reskiume/domain/usecases/SignInWithEmailAndPassword.kt
+++ composeApp/src/commonMain/kotlin/com/findmeahometeam/reskiume/domain/usecases/SignInWithEmailAndPassword.kt
@@ -3,7 +3,7 @@ package com.findmeahometeam.reskiume.domain.usecases
 import com.findmeahometeam.reskiume.data.remote.response.AuthResult
 import com.findmeahometeam.reskiume.domain.repository.AuthRepository
 
-class SignInWithEmail(private val repository: AuthRepository) {
+class SignInWithEmailAndPassword(private val repository: AuthRepository) {
 
     suspend operator fun invoke(email: String, password: String): AuthResult =
         repository.signInWithEmailAndPassword(email, password)

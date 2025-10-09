a33eedd5e0875fc024bd37ea60d2b23e447345d9
juancoob <ja.cobos.obrero@gmail.com>
2025-10-09 13:33:29 +0200

    feat: Inject context in Android

--- composeApp/src/commonMain/kotlin/com/findmeahometeam/reskiume/App.kt
+++ composeApp/src/commonMain/kotlin/com/findmeahometeam/reskiume/App.kt
@@ -1,18 +1,22 @@
 package com.findmeahometeam.reskiume
 
 import androidx.compose.runtime.Composable
-import com.findmeahometeam.reskiume.di.authModule
+import com.findmeahometeam.reskiume.di.dataModule
+import com.findmeahometeam.reskiume.di.domainModule
 import com.findmeahometeam.reskiume.di.platformModule
+import com.findmeahometeam.reskiume.di.uiModule
 import com.findmeahometeam.reskiume.ui.core.navigation.NavigationWrapper
 import org.jetbrains.compose.ui.tooling.preview.Preview
 import org.koin.compose.KoinApplication
+import org.koin.dsl.KoinAppDeclaration
 
 @Composable
 @Preview
-fun App() {
+fun App(config:KoinAppDeclaration? = null) {
     KoinApplication(application = {
+        config?.invoke(this)
         modules(
-            platformModule, authModule
+            platformModule, domainModule, dataModule, uiModule
         )
     }) {
         NavigationWrapper()

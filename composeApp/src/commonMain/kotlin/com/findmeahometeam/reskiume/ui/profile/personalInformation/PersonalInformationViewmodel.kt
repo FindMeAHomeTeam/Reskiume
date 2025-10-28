927aa53fac594b2e8010227a3665313b751b6f9d
juancoob <ja.cobos.obrero@gmail.com>
2025-10-28 10:50:26 +0100

    refactor: Store the last logout time when the user logs out

--- composeApp/src/commonMain/kotlin/com/findmeahometeam/reskiume/ui/profile/personalInformation/PersonalInformationViewmodel.kt
+++ composeApp/src/commonMain/kotlin/com/findmeahometeam/reskiume/ui/profile/personalInformation/PersonalInformationViewmodel.kt
@@ -1,19 +1,44 @@
 package com.findmeahometeam.reskiume.ui.profile.personalInformation
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.findmeahometeam.reskiume.data.remote.response.AuthUser
+import com.findmeahometeam.reskiume.data.util.Log
+import com.findmeahometeam.reskiume.domain.model.User
+import com.findmeahometeam.reskiume.domain.usecases.GetUserFromLocalDataSource
+import com.findmeahometeam.reskiume.domain.usecases.ModifyUserFromLocalDataSource
+import com.findmeahometeam.reskiume.domain.usecases.ObserveAuthStateFromAuthDataSource
 import com.findmeahometeam.reskiume.domain.usecases.SignOutFromAuthDataSource
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.launch
+import kotlin.time.Clock
+import kotlin.time.ExperimentalTime
 
 class PersonalInformationViewmodel(
+    observeAuthStateFromAuthDataSource: ObserveAuthStateFromAuthDataSource,
+    private val getUserFromLocalDataSource: GetUserFromLocalDataSource,
+    private val modifyUserFromLocalDataSource: ModifyUserFromLocalDataSource,
     private val signOutFromAuthDataSource: SignOutFromAuthDataSource
 ): ViewModel() {
 
+    private val authUserState: Flow<AuthUser?> = observeAuthStateFromAuthDataSource()
+
+    @OptIn(ExperimentalTime::class)
     fun logOut() {
-        signOutFromAuthDataSource()
-        removeUserContent()
-    }
+        viewModelScope.launch {
+            authUserState.collect { authUser: AuthUser? ->
+                if (authUser == null) return@collect
 
-    private fun removeUserContent() {
-        // TODO
+                val user: User = getUserFromLocalDataSource(authUser.uid)!!
+                modifyUserFromLocalDataSource(user.copy(lastLogout = Clock.System.now().epochSeconds)) { rowsModified: Int ->
+                    if(rowsModified > 0) {
+                        Log.d("PersonalInformationViewmodel", "logOut: lastLogout updated successfully in local data source")
+                    } else {
+                        Log.e("PersonalInformationViewmodel", "logOut: failed to update lastLogout in local data source")
+                    }
+                    signOutFromAuthDataSource()
+                }
+            }
+        }
     }
-
 }

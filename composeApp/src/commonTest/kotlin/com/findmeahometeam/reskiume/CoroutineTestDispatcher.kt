4b3d0a959f3881874cb8c15fa541fef990aaec78
juancoob <ja.cobos.obrero@gmail.com>
2025-11-11 16:51:29 +0100

    test: Create HomeViewmodelTest

---
+++ composeApp/src/commonTest/kotlin/com/findmeahometeam/reskiume/CoroutineTestDispatcher.kt
@@ -0,0 +1,25 @@
+package com.findmeahometeam.reskiume
+
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.StandardTestDispatcher
+import kotlinx.coroutines.test.TestDispatcher
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.setMain
+import kotlin.test.AfterTest
+import kotlin.test.BeforeTest
+
+@OptIn(ExperimentalCoroutinesApi::class)
+open class CoroutineTestDispatcher(
+    internal val testDispatcher: TestDispatcher = StandardTestDispatcher()
+) {
+    @BeforeTest
+    fun setUp() {
+        Dispatchers.setMain(testDispatcher)
+    }
+
+    @AfterTest
+    fun tearDown() {
+        Dispatchers.resetMain()
+    }
+}
---
+++ composeApp/src/commonTest/kotlin/com/findmeahometeam/reskiume/ui/HomeViewmodelTest.kt
@@ -0,0 +1,82 @@
+package com.findmeahometeam.reskiume.ui
+
+import app.cash.turbine.test
+import com.findmeahometeam.reskiume.CoroutineTestDispatcher
+import com.findmeahometeam.reskiume.authUser
+import com.findmeahometeam.reskiume.data.util.log.Log
+import com.findmeahometeam.reskiume.domain.repository.local.LocalRepository
+import com.findmeahometeam.reskiume.domain.repository.remote.auth.AuthRepository
+import com.findmeahometeam.reskiume.domain.usecases.GetUserFromLocalDataSource
+import com.findmeahometeam.reskiume.domain.usecases.ObserveAuthStateFromAuthDataSource
+import com.findmeahometeam.reskiume.ui.core.components.UiState
+import com.findmeahometeam.reskiume.ui.home.HomeViewmodel
+import com.findmeahometeam.reskiume.user
+import dev.mokkery.answering.returns
+import dev.mokkery.every
+import dev.mokkery.everySuspend
+import dev.mokkery.matcher.any
+import dev.mokkery.mock
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.test.runTest
+import kotlin.test.Test
+import kotlin.test.assertTrue
+
+class HomeViewmodelTest : CoroutineTestDispatcher() {
+
+    private val localRepository: LocalRepository = mock {
+        everySuspend { getUser(user.uid) } returns user
+        everySuspend { getUser("wrongUid") } returns null
+    }
+    private val log: Log = mock {
+        every { d(any(), any()) } returns Unit
+        every { e(any(), any()) } returns Unit
+    }
+
+    private val getUserFromLocalDataSource = GetUserFromLocalDataSource(localRepository)
+
+    private fun getHomeViewmodel(authRepository: AuthRepository): HomeViewmodel {
+        val observeAuthStateFromAuthDataSource = ObserveAuthStateFromAuthDataSource(authRepository)
+        return HomeViewmodel(
+            observeAuthStateFromAuthDataSource,
+            log,
+            getUserFromLocalDataSource
+        )
+    }
+
+    @Test
+    fun `given a registered user_when the user opens the app_then that user can see the chats section`() =
+        runTest {
+            val authRepository: AuthRepository = mock {
+                everySuspend { authState } returns (flowOf(authUser))
+            }
+            getHomeViewmodel(authRepository).state.test {
+                assertTrue { awaitItem() is UiState.Idle }
+                assertTrue { awaitItem() is UiState.Success }
+                ensureAllEventsConsumed()
+            }
+        }
+
+    @Test
+    fun `given a registered user on auth but not on the local data source_when the user opens the app_then that user will see the default sections`() =
+        runTest {
+            val authRepository: AuthRepository = mock {
+                everySuspend { authState } returns (flowOf(authUser.copy(uid = "wrongUid")))
+            }
+            getHomeViewmodel(authRepository).state.test {
+                assertTrue { awaitItem() is UiState.Idle }
+                ensureAllEventsConsumed()
+            }
+        }
+
+    @Test
+    fun `given an unregistered user_when the user opens the app_then that user will see the default sections`() =
+        runTest {
+            val authRepository: AuthRepository = mock {
+                every { authState } returns flowOf(null)
+            }
+            getHomeViewmodel(authRepository).state.test {
+                assertTrue { awaitItem() is UiState.Idle }
+                ensureAllEventsConsumed()
+            }
+        }
+}

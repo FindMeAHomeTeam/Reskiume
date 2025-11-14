d5f2866b93d4d594a7efadc35abd1c6fa0173397
juancoob <ja.cobos.obrero@gmail.com>
2025-11-14 12:18:10 +0100

    test: Refactor CoroutineTestDispatcher

--- composeApp/src/commonTest/kotlin/com/findmeahometeam/reskiume/CoroutineTestDispatcher.kt
+++ composeApp/src/commonTest/kotlin/com/findmeahometeam/reskiume/CoroutineTestDispatcher.kt
@@ -3,19 +3,16 @@ package com.findmeahometeam.reskiume
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.StandardTestDispatcher
-import kotlinx.coroutines.test.TestDispatcher
 import kotlinx.coroutines.test.resetMain
 import kotlinx.coroutines.test.setMain
 import kotlin.test.AfterTest
 import kotlin.test.BeforeTest
 
 @OptIn(ExperimentalCoroutinesApi::class)
-open class CoroutineTestDispatcher(
-    private val testDispatcher: TestDispatcher = StandardTestDispatcher()
-) {
+open class CoroutineTestDispatcher() {
     @BeforeTest
     fun setUp() {
-        Dispatchers.setMain(testDispatcher)
+        Dispatchers.setMain(StandardTestDispatcher())
     }
 
     @AfterTest

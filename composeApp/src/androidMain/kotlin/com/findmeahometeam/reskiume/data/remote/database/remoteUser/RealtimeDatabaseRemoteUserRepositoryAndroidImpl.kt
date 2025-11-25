7f4644a244d21c5de1b3efc3a5784cada4a756aa
juancoob <ja.cobos.obrero@gmail.com>
2025-11-25 17:30:52 +0100

    refactor: Injected the database reference in RealtimeDatabaseRemoteUserRepositoryAndroidImpl

--- composeApp/src/androidMain/kotlin/com/findmeahometeam/reskiume/data/remote/database/remoteUser/RealtimeDatabaseRemoteUserRepositoryAndroidImpl.kt
+++ composeApp/src/androidMain/kotlin/com/findmeahometeam/reskiume/data/remote/database/remoteUser/RealtimeDatabaseRemoteUserRepositoryAndroidImpl.kt
@@ -5,24 +5,19 @@ import com.findmeahometeam.reskiume.data.remote.response.RemoteUser
 import com.findmeahometeam.reskiume.data.util.Section
 import com.findmeahometeam.reskiume.data.util.log.Log
 import com.findmeahometeam.reskiume.domain.repository.remote.database.remoteUser.RealtimeDatabaseRemoteUserRepository
-import com.google.firebase.Firebase
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.ValueEventListener
-import com.google.firebase.database.database
 import com.google.firebase.database.getValue
 import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.callbackFlow
 
 class RealtimeDatabaseRemoteUserRepositoryAndroidImpl(
+    private val databaseRef: DatabaseReference,
     private val log: Log
 ) : RealtimeDatabaseRemoteUserRepository {
-
-    private val databaseRef: DatabaseReference =
-        Firebase.database.also { it.setPersistenceEnabled(true) }.reference
-
 
     override suspend fun insertRemoteUser(
         remoteUser: RemoteUser,

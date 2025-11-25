feb68e9c3078cfc90f4ccc8745d21dd5ed85bc13
juancoob <ja.cobos.obrero@gmail.com>
2025-11-25 17:24:47 +0100

    refactor: Remove unnecessary method in LocalCacheEntity

--- composeApp/src/commonMain/kotlin/com/findmeahometeam/reskiume/data/database/entity/LocalCacheEntity.kt
+++ composeApp/src/commonMain/kotlin/com/findmeahometeam/reskiume/data/database/entity/LocalCacheEntity.kt
@@ -3,7 +3,6 @@ package com.findmeahometeam.reskiume.data.database.entity
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import com.findmeahometeam.reskiume.data.util.Section
-import com.findmeahometeam.reskiume.domain.model.LocalCache
 
 @Entity
 data class LocalCacheEntity(
@@ -11,13 +10,4 @@ data class LocalCacheEntity(
     val uid: String,
     val section: Section,
     val timestamp: Long,
-) {
-    fun toDomain(): LocalCache {
-        return LocalCache(
-            id = id,
-            uid = uid,
-            section = section,
-            timestamp = timestamp
-        )
-    }
-}
+)

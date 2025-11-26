65ba245173705973ca5114cdd38ea5b23438a30d
juancoob <ja.cobos.obrero@gmail.com>
2025-11-26 12:35:56 +0100

    test: Update DeleteAccountViewmodelIntegrationTest

--- composeApp/src/commonTest/kotlin/com/findmeahometeam/reskiume/ui/integration/DeleteAccountViewmodelIntegrationTest.kt
+++ composeApp/src/commonTest/kotlin/com/findmeahometeam/reskiume/ui/integration/DeleteAccountViewmodelIntegrationTest.kt
@@ -5,8 +5,11 @@ import com.findmeahometeam.reskiume.CoroutineTestDispatcher
 import com.findmeahometeam.reskiume.authUser
 import com.findmeahometeam.reskiume.data.util.Section
 import com.findmeahometeam.reskiume.data.util.log.Log
+import com.findmeahometeam.reskiume.domain.repository.local.LocalCacheRepository
+import com.findmeahometeam.reskiume.domain.repository.local.LocalReviewRepository
 import com.findmeahometeam.reskiume.domain.repository.local.LocalUserRepository
 import com.findmeahometeam.reskiume.domain.repository.remote.auth.AuthRepository
+import com.findmeahometeam.reskiume.domain.repository.remote.database.remoteReview.RealtimeDatabaseRemoteReviewRepository
 import com.findmeahometeam.reskiume.domain.repository.remote.database.remoteUser.RealtimeDatabaseRemoteUserRepository
 import com.findmeahometeam.reskiume.domain.repository.remote.storage.StorageRepository
 import com.findmeahometeam.reskiume.domain.usecases.DeleteImageFromRemoteDataSource
@@ -17,10 +20,19 @@ import com.findmeahometeam.reskiume.domain.usecases.DeleteUserFromRemoteDataSour
 import com.findmeahometeam.reskiume.domain.usecases.GetUserFromLocalDataSource
 import com.findmeahometeam.reskiume.domain.usecases.GetUserFromRemoteDataSource
 import com.findmeahometeam.reskiume.domain.usecases.ObserveAuthStateFromAuthDataSource
+import com.findmeahometeam.reskiume.domain.usecases.localCache.DeleteCacheFromLocalRepository
+import com.findmeahometeam.reskiume.domain.usecases.review.DeleteReviewsFromLocalRepository
+import com.findmeahometeam.reskiume.domain.usecases.review.DeleteReviewsFromRemoteRepository
+import com.findmeahometeam.reskiume.domain.usecases.review.GetReviewsFromRemoteRepository
+import com.findmeahometeam.reskiume.localCache
+import com.findmeahometeam.reskiume.review
 import com.findmeahometeam.reskiume.ui.core.components.UiState
 import com.findmeahometeam.reskiume.ui.integration.fakes.FakeAuthRepository
+import com.findmeahometeam.reskiume.ui.integration.fakes.FakeLocalCacheRepository
+import com.findmeahometeam.reskiume.ui.integration.fakes.FakeLocalReviewRepository
 import com.findmeahometeam.reskiume.ui.integration.fakes.FakeLocalUserRepository
 import com.findmeahometeam.reskiume.ui.integration.fakes.FakeLog
+import com.findmeahometeam.reskiume.ui.integration.fakes.FakeRealtimeDatabaseRemoteReviewRepository
 import com.findmeahometeam.reskiume.ui.integration.fakes.FakeRealtimeDatabaseRemoteUserRepository
 import com.findmeahometeam.reskiume.ui.integration.fakes.FakeStorageRepository
 import com.findmeahometeam.reskiume.ui.profile.deleteAccount.DeleteAccountViewmodel
@@ -34,6 +46,9 @@ class DeleteAccountViewmodelIntegrationTest : CoroutineTestDispatcher() {
 
     private fun getDeleteAccountViewmodel(
         authRepository: AuthRepository = FakeAuthRepository(),
+        realtimeDatabaseRemoteReviewRepository: RealtimeDatabaseRemoteReviewRepository = FakeRealtimeDatabaseRemoteReviewRepository(),
+        localReviewRepository: LocalReviewRepository = FakeLocalReviewRepository(),
+        localCacheRepository: LocalCacheRepository = FakeLocalCacheRepository(),
         localUserRepository: LocalUserRepository = FakeLocalUserRepository(),
         realtimeDatabaseRemoteUserRepository: RealtimeDatabaseRemoteUserRepository = FakeRealtimeDatabaseRemoteUserRepository(),
         storageRepository: StorageRepository = FakeStorageRepository()
@@ -41,6 +56,17 @@ class DeleteAccountViewmodelIntegrationTest : CoroutineTestDispatcher() {
 
         val observeAuthStateFromAuthDataSource =
             ObserveAuthStateFromAuthDataSource(authRepository)
+
+        val getReviewsFromRemoteRepository =
+            GetReviewsFromRemoteRepository(realtimeDatabaseRemoteReviewRepository)
+
+        val deleteReviewsFromRemoteRepository =
+            DeleteReviewsFromRemoteRepository(realtimeDatabaseRemoteReviewRepository)
+
+        val deleteReviewsFromLocalRepository =
+            DeleteReviewsFromLocalRepository(localReviewRepository)
+
+        val deleteCacheFromLocalRepository = DeleteCacheFromLocalRepository(localCacheRepository)
 
         val getUserFromLocalDataSource =
             GetUserFromLocalDataSource(localUserRepository)
@@ -67,6 +93,10 @@ class DeleteAccountViewmodelIntegrationTest : CoroutineTestDispatcher() {
 
         return DeleteAccountViewmodel(
             observeAuthStateFromAuthDataSource,
+            getReviewsFromRemoteRepository,
+            deleteReviewsFromRemoteRepository,
+            deleteReviewsFromLocalRepository,
+            deleteCacheFromLocalRepository,
             getUserFromLocalDataSource,
             getUserFromRemoteDataSource,
             deleteUserFromAuthDataSource,
@@ -82,12 +112,54 @@ class DeleteAccountViewmodelIntegrationTest : CoroutineTestDispatcher() {
     fun `given a registered user_when that user deletes their account using their password_then their account is deleted`() =
         runTest {
             val deleteAccountViewmodel = getDeleteAccountViewmodel(
+                realtimeDatabaseRemoteReviewRepository = FakeRealtimeDatabaseRemoteReviewRepository(
+                    mutableListOf(review.toData())
+                ),
+                localReviewRepository = FakeLocalReviewRepository(mutableListOf(review.toEntity())),
+                localCacheRepository = FakeLocalCacheRepository(mutableListOf(localCache.toEntity())),
                 authRepository = FakeAuthRepository(
                     authUser = authUser,
                     authEmail = user.email,
                     authPassword = userPwd
                 ),
-                realtimeDatabaseRemoteUserRepository = FakeRealtimeDatabaseRemoteUserRepository(mutableListOf(user.toData())),
+                realtimeDatabaseRemoteUserRepository = FakeRealtimeDatabaseRemoteUserRepository(
+                    mutableListOf(user.toData())
+                ),
+                localUserRepository = FakeLocalUserRepository(mutableListOf(user)),
+                storageRepository = FakeStorageRepository(
+                    remoteDatasourceList = mutableListOf(
+                        Pair("${user.uid}/${Section.USERS.path}", user.image)
+                    ),
+                    localDatasourceList = mutableListOf(
+                        Pair(
+                            "${user.uid}/${user.image}",
+                            "local_path/${user.uid}/${Section.USERS.path}"
+                        )
+                    )
+                )
+            )
+            deleteAccountViewmodel.deleteAccount(userPwd)
+            deleteAccountViewmodel.state.test {
+                assertTrue { awaitItem() is UiState.Idle }
+                assertTrue { awaitItem() is UiState.Loading }
+                assertTrue { awaitItem() is UiState.Success }
+                ensureAllEventsConsumed()
+            }
+        }
+
+    @Test
+    fun `given a registered user_when that user deletes their account using their password and have no reviews_then their account is deleted`() =
+        runTest {
+            val deleteAccountViewmodel = getDeleteAccountViewmodel(
+                localCacheRepository = FakeLocalCacheRepository(mutableListOf(localCache.toEntity())),
+                authRepository = FakeAuthRepository(
+                    authUser = authUser,
+                    authEmail = user.email,
+                    authPassword = userPwd
+                ),
+                realtimeDatabaseRemoteUserRepository = FakeRealtimeDatabaseRemoteUserRepository(
+                    mutableListOf(user.toData())
+                ),
                 localUserRepository = FakeLocalUserRepository(mutableListOf(user)),
                 storageRepository = FakeStorageRepository(
                     remoteDatasourceList = mutableListOf(
@@ -150,7 +222,9 @@ class DeleteAccountViewmodelIntegrationTest : CoroutineTestDispatcher() {
                     authEmail = user.email,
                     authPassword = userPwd
                 ),
-                realtimeDatabaseRemoteUserRepository = FakeRealtimeDatabaseRemoteUserRepository(mutableListOf(user.toData()))
+                realtimeDatabaseRemoteUserRepository = FakeRealtimeDatabaseRemoteUserRepository(
+                    mutableListOf(user.toData())
+                )
             )
             deleteAccountViewmodel.deleteAccount(userPwd)
             deleteAccountViewmodel.state.test {

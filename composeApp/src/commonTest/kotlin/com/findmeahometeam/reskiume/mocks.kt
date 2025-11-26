276284812bfa8c452bb965f07f7d82bb0e0b1d1b
juancoob <ja.cobos.obrero@gmail.com>
2025-11-26 12:32:59 +0100

    test: Update mocks

--- composeApp/src/commonTest/kotlin/com/findmeahometeam/reskiume/mocks.kt
+++ composeApp/src/commonTest/kotlin/com/findmeahometeam/reskiume/mocks.kt
@@ -1,7 +1,12 @@
 package com.findmeahometeam.reskiume
 
 import com.findmeahometeam.reskiume.data.remote.response.AuthUser
+import com.findmeahometeam.reskiume.data.util.Section
+import com.findmeahometeam.reskiume.domain.model.LocalCache
+import com.findmeahometeam.reskiume.domain.model.Review
 import com.findmeahometeam.reskiume.domain.model.User
+
+// Mocked user data for testing
 
 val user = User(
     uid = "userUid123",
@@ -23,3 +28,26 @@ val authUser = AuthUser(
     email = user.email,
     photoUrl = user.image
 )
+
+
+// Mocked cache data for testing
+
+val localCache = LocalCache(
+    id = 1,
+    uid = user.uid,
+    section = Section.REVIEWS,
+    timestamp = 1625328000000L
+)
+
+
+// Mocked review data for testing
+
+val review = Review(
+    id = "1625241600000authorUid456",
+    timestamp = 1625241600000L,
+    authorUid = "authorUid456",
+    reviewedUid = user.uid,
+    description = "Great experience working with Juan!",
+    rating = 4.5f
+)
+

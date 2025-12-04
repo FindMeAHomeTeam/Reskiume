d69ecda8c12c1cbe53b71b035f02b540edd929f7
juancoob <ja.cobos.obrero@gmail.com>
2025-12-04 11:24:36 +0100

    test: Add FakeSaveStateHandleProvider

---
+++ composeApp/src/commonTest/kotlin/com/findmeahometeam/reskiume/ui/integrationTests/fakes/FakeSaveStateHandleProvider.kt
@@ -0,0 +1,20 @@
+package com.findmeahometeam.reskiume.ui.integrationTests.fakes
+
+import androidx.navigation.NavType
+import com.findmeahometeam.reskiume.ui.core.navigation.CheckReviews
+import com.findmeahometeam.reskiume.ui.core.navigation.SaveStateHandleProvider
+import com.findmeahometeam.reskiume.user
+import kotlin.jvm.JvmSuppressWildcards
+import kotlin.reflect.KClass
+import kotlin.reflect.KType
+
+class FakeSaveStateHandleProvider(
+    private val objectRoute: Any = CheckReviews(user.uid)
+): SaveStateHandleProvider {
+    override fun <T : Any> provideObjectRoute(
+        route: KClass<T>,
+        typeMap: Map<KType, @JvmSuppressWildcards NavType<*>>
+    ): T {
+        return objectRoute as T
+    }
+}

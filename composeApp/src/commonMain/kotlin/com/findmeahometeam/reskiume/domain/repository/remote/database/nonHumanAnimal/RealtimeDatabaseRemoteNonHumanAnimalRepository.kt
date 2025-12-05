0bd71679ad935669cba48975c568e213e9bfac5b
juancoob <ja.cobos.obrero@gmail.com>
2025-12-05 12:55:12 +0100

    refactor: Update indent for RealtimeDatabaseRemoteNonHumanAnimalRepository

--- composeApp/src/commonMain/kotlin/com/findmeahometeam/reskiume/domain/repository/remote/database/nonHumanAnimal/RealtimeDatabaseRemoteNonHumanAnimalRepository.kt
+++ composeApp/src/commonMain/kotlin/com/findmeahometeam/reskiume/domain/repository/remote/database/nonHumanAnimal/RealtimeDatabaseRemoteNonHumanAnimalRepository.kt
@@ -5,10 +5,27 @@ import com.findmeahometeam.reskiume.data.remote.response.RemoteNonHumanAnimal
 import kotlinx.coroutines.flow.Flow
 
 interface RealtimeDatabaseRemoteNonHumanAnimalRepository {
-    suspend fun insertRemoteNonHumanAnimal(remoteNonHumanAnimal: RemoteNonHumanAnimal, onInsertRemoteNonHumanAnimal: (result: DatabaseResult) -> Unit)
-    suspend fun modifyRemoteNonHumanAnimal(remoteNonHumanAnimal: RemoteNonHumanAnimal, onModifyRemoteNonHumanAnimal: (result: DatabaseResult) -> Unit)
-    suspend fun deleteRemoteNonHumanAnimal(id: String, caregiverId: String, onDeleteRemoteNonHumanAnimal: (result: DatabaseResult) -> Unit)
-    suspend fun deleteAllRemoteNonHumanAnimals(caregiverId: String, onDeleteAllRemoteNonHumanAnimals: (result: DatabaseResult) -> Unit)
+    suspend fun insertRemoteNonHumanAnimal(
+        remoteNonHumanAnimal: RemoteNonHumanAnimal,
+        onInsertRemoteNonHumanAnimal: (result: DatabaseResult) -> Unit
+    )
+
+    suspend fun modifyRemoteNonHumanAnimal(
+        remoteNonHumanAnimal: RemoteNonHumanAnimal,
+        onModifyRemoteNonHumanAnimal: (result: DatabaseResult) -> Unit
+    )
+
+    suspend fun deleteRemoteNonHumanAnimal(
+        id: String,
+        caregiverId: String,
+        onDeleteRemoteNonHumanAnimal: (result: DatabaseResult) -> Unit
+    )
+
+    suspend fun deleteAllRemoteNonHumanAnimals(
+        caregiverId: String,
+        onDeleteAllRemoteNonHumanAnimals: (result: DatabaseResult) -> Unit
+    )
+
     fun getRemoteNonHumanAnimal(id: String, caregiverId: String): Flow<RemoteNonHumanAnimal?>
     fun getAllRemoteNonHumanAnimals(caregiverId: String): Flow<List<RemoteNonHumanAnimal>>
 }

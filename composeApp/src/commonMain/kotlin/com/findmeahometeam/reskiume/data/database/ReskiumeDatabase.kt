c3a249fcdaa3b5ca2241853ac0e30e02de081ae5
juancoob <ja.cobos.obrero@gmail.com>
2025-12-05 10:30:24 +0100

    feat: Create the database for non human animals

--- composeApp/src/commonMain/kotlin/com/findmeahometeam/reskiume/data/database/ReskiumeDatabase.kt
+++ composeApp/src/commonMain/kotlin/com/findmeahometeam/reskiume/data/database/ReskiumeDatabase.kt
@@ -5,6 +5,7 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.RoomDatabaseConstructor
 import com.findmeahometeam.reskiume.data.database.dao.LocalCacheDao
+import com.findmeahometeam.reskiume.data.database.dao.NonHumanAnimalDao
 import com.findmeahometeam.reskiume.data.database.dao.ReviewDao
 import com.findmeahometeam.reskiume.data.database.dao.UserDao
 import com.findmeahometeam.reskiume.data.database.entity.LocalCacheEntity
@@ -17,7 +18,8 @@ const val DATABASE_NAME = "reskiume_database.db"
     entities = [
         LocalCacheEntity::class,
         UserEntity::class,
-        ReviewEntity::class
+        ReviewEntity::class,
+        NonHumanAnimalDao::class
     ], version = 1
 )
 @ConstructedBy(ReskiumeConstructor::class)
@@ -25,6 +27,7 @@ abstract class ReskiumeDatabase : RoomDatabase() {
     abstract fun getLocalCacheDao(): LocalCacheDao
     abstract fun getUserDao(): UserDao
     abstract fun getReviewDao(): ReviewDao
+    abstract fun getNonHumanAnimalDao(): NonHumanAnimalDao
 }
 
 // The Room compiler generates the `actual` implementations.
---
+++ composeApp/src/commonMain/kotlin/com/findmeahometeam/reskiume/data/database/dao/NonHumanAnimalDao.kt
@@ -0,0 +1,28 @@
+package com.findmeahometeam.reskiume.data.database.dao
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy.Companion.REPLACE
+import androidx.room.Query
+import androidx.room.Update
+import com.findmeahometeam.reskiume.data.database.entity.NonHumanAnimalEntity
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface NonHumanAnimalDao {
+
+    @Insert(onConflict = REPLACE)
+    suspend fun insertNonHumanAnimal(nonHumanAnimalEntity: NonHumanAnimalEntity): Long
+
+    @Update
+    suspend fun modifyNonHumanAnimal(nonHumanAnimalEntity: NonHumanAnimalEntity): Int
+
+    @Query("DELETE FROM NonHumanAnimalEntity WHERE id = :id AND caregiverId = :caregiverId")
+    suspend fun deleteNonHumanAnimal(id: String, caregiverId: String): Int
+
+    @Query("DELETE FROM NonHumanAnimalEntity WHERE caregiverId = :caregiverId OR savedBy = :caregiverId OR savedBy = '' ")
+    suspend fun deleteAllNonHumanAnimals(id: String, caregiverId: String): Int
+
+    @Query("SELECT * FROM NonHumanAnimalEntity WHERE id = :id AND caregiverId = :caregiverId")
+    fun getNonHumanAnimal(id: String, caregiverId: String): Flow<NonHumanAnimalEntity?>
+}

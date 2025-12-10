ac7f3f736655b644ea5e72cfd8c0781232b14b92
juancoob <ja.cobos.obrero@gmail.com>
2025-12-10 09:26:28 +0100

    refactor: Update StorageRepository to manage images from other sections

--- composeApp/src/commonMain/kotlin/com/findmeahometeam/reskiume/domain/repository/remote/storage/StorageRepository.kt
+++ composeApp/src/commonMain/kotlin/com/findmeahometeam/reskiume/domain/repository/remote/storage/StorageRepository.kt
@@ -3,8 +3,8 @@ package com.findmeahometeam.reskiume.domain.repository.remote.storage
 import com.findmeahometeam.reskiume.data.util.Section
 
 interface StorageRepository {
-    fun uploadImage(userUid: String, section: Section, imageUri: String, onImageUploaded: (String) -> Unit)
-    fun downloadImage(userUid: String, section: Section, onImageSaved: (String) -> Unit)
-    fun deleteLocalImage(userUid: String, currentImagePath: String, onImageDeleted: (Boolean) -> Unit)
-    suspend fun deleteRemoteImage(userUid: String, section: Section, onImageDeleted: (Boolean) -> Unit)
+    fun uploadImage(userUid: String, extraId: String = "", section: Section, imageUri: String, onImageUploaded: (String) -> Unit)
+    fun downloadImage(userUid: String, extraId: String = "", section: Section, onImageSaved: (String) -> Unit)
+    fun deleteLocalImage(currentImagePath: String, onImageDeleted: (Boolean) -> Unit)
+    suspend fun deleteRemoteImage(userUid: String, extraId: String = "", section: Section, onImageDeleted: (Boolean) -> Unit)
 }

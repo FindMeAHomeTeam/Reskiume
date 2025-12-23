b1c594d7e64b05e79f8ea14d25a3408a1b905b36
juancoob <ja.cobos.obrero@gmail.com>
2025-12-23 22:57:37 +0100

    refactor: Add extension functions to enums to get the string resource for each element

--- composeApp/src/commonMain/kotlin/com/findmeahometeam/reskiume/domain/model/Gender.kt
+++ composeApp/src/commonMain/kotlin/com/findmeahometeam/reskiume/domain/model/Gender.kt
@@ -1,5 +1,19 @@
 package com.findmeahometeam.reskiume.domain.model
 
+import org.jetbrains.compose.resources.StringResource
+import reskiume.composeapp.generated.resources.Res
+import reskiume.composeapp.generated.resources.non_human_animal_gender_female
+import reskiume.composeapp.generated.resources.non_human_animal_gender_male
+import reskiume.composeapp.generated.resources.non_human_animal_gender_unselected
+
 enum class Gender {
     UNSELECTED, FEMALE, MALE
+}
+
+fun Gender.toStringResource(): StringResource {
+    return when(this) {
+        Gender.UNSELECTED -> Res.string.non_human_animal_gender_unselected
+        Gender.FEMALE -> Res.string.non_human_animal_gender_female
+        Gender.MALE -> Res.string.non_human_animal_gender_male
+    }
 }

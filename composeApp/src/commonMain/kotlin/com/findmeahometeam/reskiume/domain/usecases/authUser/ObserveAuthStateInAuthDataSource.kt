664d69afaf984851214c3047d23492e7a2dd6215
juancoob <ja.cobos.obrero@gmail.com>
2025-12-29 13:32:49 +0100

    refactor: Specify the type to ObserveAuthStateInAuthDataSource

--- composeApp/src/commonMain/kotlin/com/findmeahometeam/reskiume/domain/usecases/authUser/ObserveAuthStateInAuthDataSource.kt
+++ composeApp/src/commonMain/kotlin/com/findmeahometeam/reskiume/domain/usecases/authUser/ObserveAuthStateInAuthDataSource.kt
@@ -1,7 +1,9 @@
 package com.findmeahometeam.reskiume.domain.usecases.authUser
 
+import com.findmeahometeam.reskiume.data.remote.response.AuthUser
 import com.findmeahometeam.reskiume.domain.repository.remote.auth.AuthRepository
+import kotlinx.coroutines.flow.Flow
 
 class ObserveAuthStateInAuthDataSource(private val repository: AuthRepository) {
-    operator fun invoke() = repository.authState
+    operator fun invoke(): Flow<AuthUser?> = repository.authState
 }

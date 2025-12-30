d3a26c3d4ed8301b365769c6030ad73d71bff621
juancoob <ja.cobos.obrero@gmail.com>
2025-12-30 16:52:57 +0100

    feat: Create DeleteNonHumanAnimalUtil to add the delete non human animal methods

--- composeApp/src/commonMain/kotlin/com/findmeahometeam/reskiume/di/UiModule.kt
+++ composeApp/src/commonMain/kotlin/com/findmeahometeam/reskiume/di/UiModule.kt
@@ -5,14 +5,15 @@ import com.findmeahometeam.reskiume.ui.core.navigation.SaveStateHandleProviderIm
 import com.findmeahometeam.reskiume.ui.home.HomeViewmodel
 import com.findmeahometeam.reskiume.ui.profile.ProfileViewmodel
 import com.findmeahometeam.reskiume.ui.profile.checkAllNonHumanAnimals.CheckAllNonHumanAnimalsViewmodel
-import com.findmeahometeam.reskiume.ui.profile.checkNonHumanAnimal.CheckNonHumanAnimalUtil
 import com.findmeahometeam.reskiume.ui.profile.checkNonHumanAnimal.CheckNonHumanAnimalViewmodel
+import com.findmeahometeam.reskiume.ui.profile.checkNonHumanAnimal.CheckNonHumanAnimalUtil
 import com.findmeahometeam.reskiume.ui.profile.createAccount.CreateAccountViewmodel
 import com.findmeahometeam.reskiume.ui.profile.deleteAccount.DeleteAccountViewmodel
 import com.findmeahometeam.reskiume.ui.profile.loginAccount.LoginAccountViewmodel
 import com.findmeahometeam.reskiume.ui.profile.modifyAccount.ModifyAccountViewmodel
 import com.findmeahometeam.reskiume.ui.profile.checkReviews.CheckReviewsViewmodel
 import com.findmeahometeam.reskiume.ui.profile.createNonHumanAnimal.CreateNonHumanAnimalViewmodel
+import com.findmeahometeam.reskiume.ui.profile.modifyNonHumanAnimal.DeleteNonHumanAnimalUtil
 import com.findmeahometeam.reskiume.ui.profile.modifyNonHumanAnimal.ModifyNonHumanAnimalViewmodel
 import org.koin.core.module.dsl.factoryOf
 import org.koin.core.module.dsl.singleOf
@@ -34,4 +35,5 @@ val uiModule = module {
     viewModelOf(::CheckNonHumanAnimalViewmodel)
     singleOf(::CheckNonHumanAnimalUtil)
     viewModelOf(::CreateNonHumanAnimalViewmodel)
+    singleOf(::DeleteNonHumanAnimalUtil)
 }
---
+++ composeApp/src/commonMain/kotlin/com/findmeahometeam/reskiume/ui/profile/modifyNonHumanAnimal/DeleteNonHumanAnimalUtil.kt
@@ -0,0 +1,217 @@
+package com.findmeahometeam.reskiume.ui.profile.modifyNonHumanAnimal
+
+import com.findmeahometeam.reskiume.data.remote.response.DatabaseResult
+import com.findmeahometeam.reskiume.data.util.Section
+import com.findmeahometeam.reskiume.data.util.log.Log
+import com.findmeahometeam.reskiume.domain.model.NonHumanAnimal
+import com.findmeahometeam.reskiume.domain.usecases.image.DeleteImageFromLocalDataSource
+import com.findmeahometeam.reskiume.domain.usecases.image.DeleteImageFromRemoteDataSource
+import com.findmeahometeam.reskiume.domain.usecases.localCache.DeleteCacheFromLocalRepository
+import com.findmeahometeam.reskiume.domain.usecases.nonHumanAnimal.DeleteNonHumanAnimalFromLocalRepository
+import com.findmeahometeam.reskiume.domain.usecases.nonHumanAnimal.DeleteNonHumanAnimalFromRemoteRepository
+import com.findmeahometeam.reskiume.domain.usecases.nonHumanAnimal.GetNonHumanAnimalFromLocalRepository
+import com.findmeahometeam.reskiume.domain.usecases.nonHumanAnimal.GetNonHumanAnimalFromRemoteRepository
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
+
+class DeleteNonHumanAnimalUtil(
+    private val getNonHumanAnimalFromRemoteRepository: GetNonHumanAnimalFromRemoteRepository,
+    private val getNonHumanAnimalFromLocalRepository: GetNonHumanAnimalFromLocalRepository,
+    private val deleteImageFromRemoteDataSource: DeleteImageFromRemoteDataSource,
+    private val deleteImageFromLocalDataSource: DeleteImageFromLocalDataSource,
+    private val deleteNonHumanAnimalFromRemoteRepository: DeleteNonHumanAnimalFromRemoteRepository,
+    private val deleteNonHumanAnimalFromLocalRepository: DeleteNonHumanAnimalFromLocalRepository,
+    private val deleteCacheFromLocalRepository: DeleteCacheFromLocalRepository,
+    private val log: Log
+) {
+
+    fun deleteNonHumanAnimal(
+        id: String,
+        caregiverId: String,
+        coroutineScope: CoroutineScope,
+        onError: () -> Unit,
+        onComplete: () -> Unit
+    ) {
+        deleteCurrentImageFromRemoteDataSource(
+            caregiverId,
+            id,
+            coroutineScope,
+            onError
+        ) {
+            deleteCurrentImageFromLocalDataSource(
+                id,
+                coroutineScope,
+                onError
+            ) {
+                deleteNonHumanAnimalFromRemoteDataSource(
+                    id,
+                    caregiverId,
+                    onError
+                ) {
+                    deleteNonHumanAnimalFromLocalDataSource(
+                        id,
+                        coroutineScope,
+                        onError
+                    ) {
+                        deleteNonHumanAnimalCacheFromLocalDataSource(
+                            id,
+                            coroutineScope,
+                            onComplete
+                        )
+                    }
+                }
+            }
+        }
+    }
+
+    private fun deleteCurrentImageFromRemoteDataSource(
+        caregiverId: String,
+        nonHumanAnimalId: String,
+        coroutineScope: CoroutineScope,
+        onError: () -> Unit,
+        onSuccess: () -> Unit
+    ) {
+        coroutineScope.launch {
+
+            getNonHumanAnimalFromRemoteRepository(
+                nonHumanAnimalId,
+                caregiverId
+            ).collect { remoteNonHumanAnimal ->
+
+                if (remoteNonHumanAnimal == null) {
+                    return@collect
+                }
+                deleteImageFromRemoteDataSource(
+                    userUid = caregiverId,
+                    extraId = nonHumanAnimalId,
+                    section = Section.NON_HUMAN_ANIMALS,
+                    currentImage = remoteNonHumanAnimal.imageUrl
+                ) { isDeleted ->
+
+                    if (isDeleted) {
+                        log.d(
+                            "DeleteNonHumanAnimalUtil",
+                            "deleteCurrentImageFromRemoteDataSource: Image from the non human animal $nonHumanAnimalId was deleted successfully in remote data source"
+                        )
+                        onSuccess()
+                    } else {
+                        log.e(
+                            "DeleteNonHumanAnimalUtil",
+                            "deleteCurrentImageFromRemoteDataSource: failed to delete the image from the non human animal $nonHumanAnimalId in remote data source"
+                        )
+                        onError()
+                    }
+                }
+            }
+        }
+    }
+
+    private fun deleteCurrentImageFromLocalDataSource(
+        nonHumanAnimalId: String,
+        coroutineScope: CoroutineScope,
+        onError: () -> Unit,
+        onSuccess: () -> Unit
+    ) {
+        coroutineScope.launch {
+
+            val localNonHumanAnimal: NonHumanAnimal =
+                getNonHumanAnimalFromLocalRepository(nonHumanAnimalId)!!
+
+            deleteImageFromLocalDataSource(currentImagePath = localNonHumanAnimal.imageUrl) { isDeleted ->
+
+                if (isDeleted) {
+                    log.d(
+                        "DeleteNonHumanAnimalUtil",
+                        "deleteCurrentImageFromLocalDataSource: Image from the non human animal $nonHumanAnimalId was deleted successfully in the local data source"
+                    )
+                    onSuccess()
+                } else {
+                    log.e(
+                        "DeleteNonHumanAnimalUtil",
+                        "deleteCurrentImageFromLocalDataSource: Failed to delete the image from the non human animal $nonHumanAnimalId in the local data source"
+                    )
+                    onError()
+                }
+            }
+        }
+    }
+
+    private fun deleteNonHumanAnimalFromRemoteDataSource(
+        id: String,
+        caregiverId: String,
+        onError: () -> Unit,
+        onSuccess: () -> Unit
+    ) {
+        deleteNonHumanAnimalFromRemoteRepository(
+            id,
+            caregiverId
+        ) { databaseResult: DatabaseResult ->
+
+            if (databaseResult is DatabaseResult.Success) {
+                log.d(
+                    "DeleteNonHumanAnimalUtil",
+                    "deleteNonHumanAnimalFromRemoteDataSource: Non human animal $id deleted in the remote data source"
+                )
+                onSuccess()
+            } else {
+                log.e(
+                    "DeleteNonHumanAnimalUtil",
+                    "deleteNonHumanAnimalFromRemoteDataSource: Error deleting the non human animal $id in the remote data source"
+                )
+                onError()
+            }
+        }
+    }
+
+    private fun deleteNonHumanAnimalFromLocalDataSource(
+        id: String,
+        coroutineScope: CoroutineScope,
+        onError: () -> Unit,
+        onSuccess: () -> Unit
+    ) {
+        coroutineScope.launch {
+
+            deleteNonHumanAnimalFromLocalRepository(id) { rowsDeleted: Int ->
+
+                if (rowsDeleted > 0) {
+                    log.d(
+                        "DeleteNonHumanAnimalUtil",
+                        "deleteNonHumanAnimalFromLocalDataSource: Non human animal $id deleted in the local data source"
+                    )
+                    onSuccess()
+                } else {
+                    log.e(
+                        "DeleteNonHumanAnimalUtil",
+                        "deleteNonHumanAnimalFromLocalDataSource: Error deleting the non human animal $id in the local data source"
+                    )
+                    onError()
+                }
+            }
+        }
+    }
+
+    private fun deleteNonHumanAnimalCacheFromLocalDataSource(
+        id: String,
+        coroutineScope: CoroutineScope,
+        onComplete: () -> Unit
+    ) {
+        coroutineScope.launch {
+
+            deleteCacheFromLocalRepository(id) { rowsDeleted: Int ->
+
+                if (rowsDeleted > 0) {
+                    log.d(
+                        "DeleteNonHumanAnimalUtil",
+                        "Non human animal $id deleted in the local cache in section ${Section.NON_HUMAN_ANIMALS}"
+                    )
+                } else {
+                    log.e(
+                        "DeleteNonHumanAnimalUtil",
+                        "Error deleting the non human animal $id in the local cache in section ${Section.NON_HUMAN_ANIMALS}"
+                    )
+                }
+                onComplete()
+            }
+        }
+    }
+}

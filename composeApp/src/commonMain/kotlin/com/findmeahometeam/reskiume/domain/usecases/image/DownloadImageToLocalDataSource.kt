fc4886d8ddcb3b841aad79297116b186246d1810
juancoob <ja.cobos.obrero@gmail.com>
2026-01-02 16:57:23 +0100

    refactor: Update DownloadImageToLocalDataSource to return the imagePath

--- composeApp/src/commonMain/kotlin/com/findmeahometeam/reskiume/domain/usecases/image/DownloadImageToLocalDataSource.kt
+++ composeApp/src/commonMain/kotlin/com/findmeahometeam/reskiume/domain/usecases/image/DownloadImageToLocalDataSource.kt
@@ -2,15 +2,22 @@ package com.findmeahometeam.reskiume.domain.usecases.image
 
 import com.findmeahometeam.reskiume.data.util.Section
 import com.findmeahometeam.reskiume.domain.repository.remote.storage.StorageRepository
+import kotlin.coroutines.resume
+import kotlin.coroutines.suspendCoroutine
 
 class DownloadImageToLocalDataSource(private val storageRepository: StorageRepository) {
 
-    operator fun invoke(
+    suspend operator fun invoke(
         userUid: String,
         extraId: String,
         section: Section,
-        onImageSaved: (String) -> Unit
-    ) {
-        storageRepository.downloadImage(userUid, extraId, section, onImageSaved)
+    ): String {
+        return suspendCoroutine { continuation ->
+
+            storageRepository.downloadImage(userUid, extraId, section) { imagePath ->
+
+                continuation.resume(imagePath)
+            }
+        }
     }
 }

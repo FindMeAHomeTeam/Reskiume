80a219f68bbca4393ee8ccd0a51faaf15e7bca8a
juancoob <ja.cobos.obrero@gmail.com>
2026-01-02 17:20:10 +0100

    refactor: Update the back arrow icon to button

--- composeApp/src/commonMain/kotlin/com/findmeahometeam/reskiume/ui/core/components/RmScaffold.kt
+++ composeApp/src/commonMain/kotlin/com/findmeahometeam/reskiume/ui/core/components/RmScaffold.kt
@@ -1,13 +1,13 @@
 package com.findmeahometeam.reskiume.ui.core.components
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
@@ -46,12 +46,17 @@ fun RmScaffold(
                     )
                 },
                 navigationIcon = {
-                    Icon(
-                        painter = painterResource(Res.drawable.ic_back),
-                        contentDescription = stringResource(Res.string.back_arrow_content_description),
-                        tint = textColor,
-                        modifier = Modifier.padding(16.dp).size(24.dp).clickable { onBackPressed() }
-                    )
+                    IconButton(
+                        modifier = Modifier.padding(start = 16.dp).size(32.dp),
+                        onClick = onBackPressed
+                    ) {
+                        Icon(
+                            painter = painterResource(Res.drawable.ic_back),
+                            contentDescription = stringResource(Res.string.back_arrow_content_description),
+                            tint = textColor,
+                            modifier = Modifier.size(24.dp),
+                        )
+                    }
                 },
                 colors = TopAppBarDefaults.topAppBarColors().copy(containerColor = backgroundColor)
             )

5fb24faa7f1d17fbfb59d73a155bfe847ea7e12f
juancoob <ja.cobos.obrero@gmail.com>
2026-01-07 12:18:44 +0100

    feat: Add the CheckAllAdviceScreen

---
+++ composeApp/src/commonMain/kotlin/com/findmeahometeam/reskiume/ui/core/components/RmSearchBar.kt
@@ -0,0 +1,94 @@
+package com.findmeahometeam.reskiume.ui.core.components
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.SearchBarDefaults
+import androidx.compose.material3.SearchBarDefaults.inputFieldColors
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.semantics.isTraversalGroup
+import androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.unit.dp
+import com.findmeahometeam.reskiume.ui.core.backgroundColorForItems
+import com.findmeahometeam.reskiume.ui.core.textColor
+import org.jetbrains.compose.resources.painterResource
+import org.jetbrains.compose.resources.stringResource
+import reskiume.composeapp.generated.resources.Res
+import reskiume.composeapp.generated.resources.check_all_advice_screen_erase_input_content_description
+import reskiume.composeapp.generated.resources.check_all_advice_screen_search_icon_content_description
+import reskiume.composeapp.generated.resources.check_all_advice_screen_seek_advice
+import reskiume.composeapp.generated.resources.ic_erase_input
+import reskiume.composeapp.generated.resources.ic_search
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun RmSearchBar(
+    modifier: Modifier = Modifier,
+    onSearch: (String) -> Unit
+) {
+    var query: String by rememberSaveable { mutableStateOf("") }
+    val focusManager = LocalFocusManager.current
+
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .semantics { isTraversalGroup = true }
+    ) {
+        SearchBarDefaults.InputField(
+            modifier = modifier.then(Modifier.fillMaxWidth()),
+            query = query,
+            onQueryChange = {
+                query = it
+                onSearch(it)
+            },
+            onSearch = {
+                focusManager.clearFocus()
+            },
+            expanded = false,
+            onExpandedChange = { },
+            placeholder = { RmText(stringResource(Res.string.check_all_advice_screen_seek_advice)) },
+            leadingIcon = {
+                Icon(
+                    painter = painterResource(Res.drawable.ic_search),
+                    contentDescription = stringResource(Res.string.check_all_advice_screen_search_icon_content_description),
+                    tint = textColor
+                )
+            },
+            trailingIcon = {
+                if (query.isNotEmpty()) {
+                    IconButton(
+                        modifier = Modifier.size(32.dp),
+                        onClick = {
+                            query = ""
+                            onSearch("")
+                            focusManager.clearFocus()
+                        }
+                    ) {
+                        Icon(
+                            painter = painterResource(Res.drawable.ic_erase_input),
+                            contentDescription = stringResource(Res.string.check_all_advice_screen_erase_input_content_description),
+                            tint = textColor,
+                            modifier = Modifier.size(24.dp),
+                        )
+                    }
+                }
+            },
+            colors = inputFieldColors().copy(
+                unfocusedContainerColor = backgroundColorForItems,
+                focusedContainerColor = backgroundColorForItems,
+                focusedTextColor = textColor,
+                unfocusedTextColor = textColor,
+                cursorColor = textColor
+            )
+        )
+    }
+}
---
+++ composeApp/src/commonMain/kotlin/com/findmeahometeam/reskiume/ui/profile/checkAllAdvice/CheckAllAdviceScreen.kt
@@ -0,0 +1,234 @@
+package com.findmeahometeam.reskiume.ui.profile.checkAllAdvice
+
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExtendedFloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.SegmentedButton
+import androidx.compose.material3.SegmentedButtonDefaults
+import androidx.compose.material3.SingleChoiceSegmentedButtonRow
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.findmeahometeam.reskiume.domain.model.Advice
+import com.findmeahometeam.reskiume.ui.core.backgroundColor
+import com.findmeahometeam.reskiume.ui.core.components.RmDialog
+import com.findmeahometeam.reskiume.ui.core.components.RmListAvatarType
+import com.findmeahometeam.reskiume.ui.core.components.RmListItem
+import com.findmeahometeam.reskiume.ui.core.components.RmResultState
+import com.findmeahometeam.reskiume.ui.core.components.RmScaffold
+import com.findmeahometeam.reskiume.ui.core.components.RmSearchBar
+import com.findmeahometeam.reskiume.ui.core.components.RmText
+import com.findmeahometeam.reskiume.ui.core.components.UiState
+import com.findmeahometeam.reskiume.ui.core.primaryGreen
+import com.findmeahometeam.reskiume.ui.core.secondaryGreen
+import com.findmeahometeam.reskiume.ui.core.tertiaryGreen
+import com.findmeahometeam.reskiume.ui.profile.giveFeedback.GiveFeedback
+import org.jetbrains.compose.resources.painterResource
+import org.jetbrains.compose.resources.stringResource
+import org.koin.compose.koinInject
+import org.koin.compose.viewmodel.koinViewModel
+import reskiume.composeapp.generated.resources.Res
+import reskiume.composeapp.generated.resources.check_all_advice_screen_no_advice_found
+import reskiume.composeapp.generated.resources.check_all_advice_screen_option_advice_mail_body
+import reskiume.composeapp.generated.resources.check_all_advice_screen_option_advice_mail_subject
+import reskiume.composeapp.generated.resources.check_all_advice_screen_option_send_advice_button
+import reskiume.composeapp.generated.resources.check_all_advice_screen_title
+import reskiume.composeapp.generated.resources.give_feedback_no_email_app_dialog_message
+import reskiume.composeapp.generated.resources.give_feedback_no_email_app_dialog_ok_button
+import reskiume.composeapp.generated.resources.give_feedback_no_email_app_dialog_title
+import reskiume.composeapp.generated.resources.ic_mail
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun CheckAllAdviceScreen(
+    onBackPressed: () -> Unit
+) {
+    val checkAllAdviceViewmodel: CheckAllAdviceViewmodel = koinViewModel<CheckAllAdviceViewmodel>()
+
+    val adviceListState: UiState<List<Advice>> by checkAllAdviceViewmodel.adviceListState.collectAsState()
+
+    RmScaffold(
+        title = stringResource(Res.string.check_all_advice_screen_title),
+        onBackPressed = onBackPressed,
+        floatingActionButton = {
+            DisplayExtendedFloatingActionButton(checkAllAdviceViewmodel)
+        }
+    ) { padding ->
+
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(backgroundColor)
+                .padding(padding)
+                .padding(horizontal = 16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            RmSearchBar {
+                checkAllAdviceViewmodel.searchAdvice(it)
+            }
+            Spacer(modifier = Modifier.height(8.dp))
+
+            DisplaySingleChoiceSegmentedButtonRow(checkAllAdviceViewmodel)
+            Spacer(modifier = Modifier.height(8.dp))
+
+            RmResultState(adviceListState) { adviceList: List<Advice> ->
+
+                AnimatedVisibility(
+                    visible = adviceList.isEmpty(),
+                ) {
+                    Column(
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.Center
+                    ) {
+                        Spacer(modifier = Modifier.weight(1f))
+                        RmText(
+                            modifier = Modifier.fillMaxWidth(),
+                            text = stringResource(Res.string.check_all_advice_screen_no_advice_found),
+                            textAlign = TextAlign.Center,
+                            fontSize = 18.sp,
+                            fontWeight = FontWeight.Black
+                        )
+                        Spacer(modifier = Modifier.weight(1f))
+                    }
+                }
+                if (adviceList.isNotEmpty()) {
+                    LazyColumn {
+                        items(adviceList, key = { advice -> advice.hashCode() }) { advice ->
+
+                            RmListItem(
+                                modifier = Modifier.animateItem(),
+                                title = stringResource(advice.title),
+                                description = stringResource(advice.description),
+                                listAvatarType = RmListAvatarType.Icon(
+                                    backgroundColor = advice.image.backgroundColor,
+                                    icon = advice.image.icon,
+                                    iconColor = advice.image.iconColor
+                                ),
+                                onClick = {
+                                    //
+                                }
+                            )
+                            Spacer(modifier = Modifier.height(8.dp))
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+private fun DisplaySingleChoiceSegmentedButtonRow(checkAllAdviceViewmodel: CheckAllAdviceViewmodel) {
+
+    var selectedIndex by rememberSaveable { mutableIntStateOf(0) }
+
+    SingleChoiceSegmentedButtonRow {
+
+        AdviceType.entries.forEachIndexed { index, adviceType ->
+            SegmentedButton(
+                shape = SegmentedButtonDefaults.itemShape(
+                    index = index,
+                    count = AdviceType.entries.size
+                ),
+                onClick = {
+                    selectedIndex = index
+                    checkAllAdviceViewmodel.updateAdviceList(adviceType)
+                },
+                selected = index == selectedIndex,
+                colors = SegmentedButtonDefaults.colors().copy(
+                    activeContainerColor = secondaryGreen,
+                    inactiveContainerColor = tertiaryGreen,
+                    activeBorderColor = Color.Black,
+                    inactiveBorderColor = Color.Black
+                ),
+                label = {
+                    RmText(
+                        text = stringResource(adviceType.stringResource),
+                        color = Color.Black
+                    )
+                }
+            )
+        }
+    }
+}
+
+@Composable
+private fun DisplayExtendedFloatingActionButton(checkAllAdviceViewmodel: CheckAllAdviceViewmodel) {
+
+    val giveFeedback: GiveFeedback = koinInject<GiveFeedback>()
+
+    var displaySendAdviceButton by rememberSaveable { mutableStateOf(false) }
+    var displayNoEmailAppError: Boolean by remember { mutableStateOf(false) }
+
+    checkAllAdviceViewmodel.checkAuthState { isLoggedIn ->
+        displaySendAdviceButton = isLoggedIn
+    }
+    if (displaySendAdviceButton) {
+
+        val sendAdviceSubject =
+            stringResource(Res.string.check_all_advice_screen_option_advice_mail_subject)
+        val sendAdviceBody =
+            stringResource(Res.string.check_all_advice_screen_option_advice_mail_body)
+        ExtendedFloatingActionButton(
+            icon = {
+                Icon(
+                    modifier = Modifier.size(24.dp),
+                    painter = painterResource(Res.drawable.ic_mail),
+                    contentDescription = null,
+                    tint = primaryGreen
+                )
+            },
+            text = {
+                RmText(
+                    text = stringResource(Res.string.check_all_advice_screen_option_send_advice_button),
+                    color = Color.Black
+                )
+            },
+            containerColor = tertiaryGreen,
+            onClick = {
+                giveFeedback.sendEmail(
+                    subject = sendAdviceSubject,
+                    body = sendAdviceBody,
+                    onError = {
+                        displayNoEmailAppError = true
+                    }
+                )
+            }
+        )
+        if (displayNoEmailAppError) {
+
+            RmDialog(
+                emoji = "✉️",
+                title = stringResource(Res.string.give_feedback_no_email_app_dialog_title),
+                message = stringResource(Res.string.give_feedback_no_email_app_dialog_message),
+                allowMessage = stringResource(Res.string.give_feedback_no_email_app_dialog_ok_button),
+                onClickAllow = { displayNoEmailAppError = false },
+                onClickDeny = { displayNoEmailAppError = false }
+            )
+        }
+    }
+}

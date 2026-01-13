ee40b544f083bc2bf63c12438386a265dea3751c
juancoob <ja.cobos.obrero@gmail.com>
2026-01-13 22:25:30 +0100

    feat: Add delete use cases in local database for foster homes

---
+++ composeApp/src/commonMain/kotlin/com/findmeahometeam/reskiume/domain/usecases/fosterHome/DeleteFosterHomeFromLocalRepository.kt
@@ -0,0 +1,13 @@
+package com.findmeahometeam.reskiume.domain.usecases.fosterHome
+
+import com.findmeahometeam.reskiume.domain.repository.local.LocalFosterHomeRepository
+
+class DeleteFosterHomeFromLocalRepository(private val localFosterHomeRepository: LocalFosterHomeRepository) {
+    
+    suspend operator fun invoke(
+        id: String,
+        onDeleteFosterHome: (rowsDeleted: Int) -> Unit
+    ) {
+        localFosterHomeRepository.deleteFosterHome(id, onDeleteFosterHome)
+    }
+}

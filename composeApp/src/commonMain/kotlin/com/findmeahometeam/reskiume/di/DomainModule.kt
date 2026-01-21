db5a8bef5e9ceab680e0baa5b53fa8b7ea49a56a
juancoob <ja.cobos.obrero@gmail.com>
2026-01-21 14:20:42 +0100

    feat: Inject last use cases

--- composeApp/src/commonMain/kotlin/com/findmeahometeam/reskiume/di/DomainModule.kt
+++ composeApp/src/commonMain/kotlin/com/findmeahometeam/reskiume/di/DomainModule.kt
@@ -18,17 +18,27 @@ import com.findmeahometeam.reskiume.domain.usecases.authUser.ObserveAuthStateInA
 import com.findmeahometeam.reskiume.domain.usecases.image.DownloadImageToLocalDataSource
 import com.findmeahometeam.reskiume.domain.usecases.authUser.SignInWithEmailAndPasswordFromAuthDataSource
 import com.findmeahometeam.reskiume.domain.usecases.authUser.SignOutFromAuthDataSource
-import com.findmeahometeam.reskiume.domain.usecases.fosterHome.DeleteAllFosterHomesFromLocalRepository
+import com.findmeahometeam.reskiume.domain.usecases.fosterHome.DeleteAllMyFosterHomesFromLocalRepository
+import com.findmeahometeam.reskiume.domain.usecases.fosterHome.DeleteAllMyFosterHomesFromRemoteRepository
 import com.findmeahometeam.reskiume.domain.usecases.fosterHome.DeleteFosterHomeFromLocalRepository
-import com.findmeahometeam.reskiume.domain.usecases.fosterHome.GetAllFosterHomesFromLocalRepository
+import com.findmeahometeam.reskiume.domain.usecases.fosterHome.DeleteFosterHomeFromRemoteRepository
+import com.findmeahometeam.reskiume.domain.usecases.fosterHome.GetAllFosterHomesByCountryAndCityFromLocalRepository
+import com.findmeahometeam.reskiume.domain.usecases.fosterHome.GetAllFosterHomesByCountryAndCityFromRemoteRepository
+import com.findmeahometeam.reskiume.domain.usecases.fosterHome.GetAllFosterHomesByLocationFromLocalRepository
+import com.findmeahometeam.reskiume.domain.usecases.fosterHome.GetAllFosterHomesByLocationFromRemoteRepository
+import com.findmeahometeam.reskiume.domain.usecases.fosterHome.GetAllMyFosterHomesFromLocalRepository
+import com.findmeahometeam.reskiume.domain.usecases.fosterHome.GetAllMyFosterHomesFromRemoteRepository
 import com.findmeahometeam.reskiume.domain.usecases.fosterHome.GetFosterHomeFromLocalRepository
+import com.findmeahometeam.reskiume.domain.usecases.fosterHome.GetFosterHomeFromRemoteRepository
 import com.findmeahometeam.reskiume.domain.usecases.fosterHome.InsertAcceptedNonHumanAnimalGenderForFosterHomeInLocalRepository
 import com.findmeahometeam.reskiume.domain.usecases.fosterHome.InsertAcceptedNonHumanAnimalTypeForFosterHomeInLocalRepository
 import com.findmeahometeam.reskiume.domain.usecases.fosterHome.InsertFosterHomeInLocalRepository
+import com.findmeahometeam.reskiume.domain.usecases.fosterHome.InsertFosterHomeInRemoteRepository
 import com.findmeahometeam.reskiume.domain.usecases.fosterHome.InsertResidentNonHumanAnimalForFosterHomeInLocalRepository
 import com.findmeahometeam.reskiume.domain.usecases.fosterHome.ModifyAcceptedNonHumanAnimalGenderForFosterHomeInLocalRepository
 import com.findmeahometeam.reskiume.domain.usecases.fosterHome.ModifyAcceptedNonHumanAnimalTypeForFosterHomeInLocalRepository
 import com.findmeahometeam.reskiume.domain.usecases.fosterHome.ModifyFosterHomeInLocalRepository
+import com.findmeahometeam.reskiume.domain.usecases.fosterHome.ModifyFosterHomeInRemoteRepository
 import com.findmeahometeam.reskiume.domain.usecases.fosterHome.ModifyResidentNonHumanAnimalForFosterHomeInLocalRepository
 import com.findmeahometeam.reskiume.domain.usecases.image.GetCompleteImagePathFromLocalDataSource
 import com.findmeahometeam.reskiume.domain.usecases.image.UploadImageToRemoteDataSource
@@ -74,18 +84,28 @@ val domainModule = module {
     factoryOf(::SignOutFromAuthDataSource)
 
     // fosterHome
-    factoryOf(::DeleteAllFosterHomesFromLocalRepository)
+    factoryOf(::DeleteAllMyFosterHomesFromLocalRepository)
+    factoryOf(::DeleteAllMyFosterHomesFromRemoteRepository)
     factoryOf(::DeleteFosterHomeFromLocalRepository)
-    factoryOf(::GetAllFosterHomesFromLocalRepository)
+    factoryOf(::DeleteFosterHomeFromRemoteRepository)
+    factoryOf(::GetAllMyFosterHomesFromLocalRepository)
+    factoryOf(::GetAllMyFosterHomesFromRemoteRepository)
+    factoryOf(::GetAllFosterHomesByCountryAndCityFromLocalRepository)
+    factoryOf(::GetAllFosterHomesByCountryAndCityFromRemoteRepository)
+    factoryOf(::GetAllFosterHomesByLocationFromLocalRepository)
+    factoryOf(::GetAllFosterHomesByLocationFromRemoteRepository)
     factoryOf(::GetFosterHomeFromLocalRepository)
+    factoryOf(::GetFosterHomeFromRemoteRepository)
     factoryOf(::InsertFosterHomeInLocalRepository)
     factoryOf(::InsertResidentNonHumanAnimalForFosterHomeInLocalRepository)
     factoryOf(::InsertAcceptedNonHumanAnimalTypeForFosterHomeInLocalRepository)
     factoryOf(::InsertAcceptedNonHumanAnimalGenderForFosterHomeInLocalRepository)
+    factoryOf(::InsertFosterHomeInRemoteRepository)
     factoryOf(::ModifyFosterHomeInLocalRepository)
     factoryOf(::ModifyResidentNonHumanAnimalForFosterHomeInLocalRepository)
     factoryOf(::ModifyAcceptedNonHumanAnimalTypeForFosterHomeInLocalRepository)
     factoryOf(::ModifyAcceptedNonHumanAnimalGenderForFosterHomeInLocalRepository)
+    factoryOf(::ModifyFosterHomeInRemoteRepository)
 
     // image
     factoryOf(::DeleteImageFromLocalDataSource)

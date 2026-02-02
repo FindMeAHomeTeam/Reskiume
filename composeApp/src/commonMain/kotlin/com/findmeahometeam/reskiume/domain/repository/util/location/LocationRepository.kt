24810d5cecb52d2be3e16c0d3dbe2f56d4959dce
juancoob <ja.cobos.obrero@gmail.com>
2026-02-02 10:24:44 +0100

    refactor: Update the LocationRepository to observe if the location services are enabled using flows

--- composeApp/src/commonMain/kotlin/com/findmeahometeam/reskiume/domain/repository/util/location/LocationRepository.kt
+++ composeApp/src/commonMain/kotlin/com/findmeahometeam/reskiume/domain/repository/util/location/LocationRepository.kt
@@ -1,8 +1,10 @@
 package com.findmeahometeam.reskiume.domain.repository.util.location
 
+import kotlinx.coroutines.flow.Flow
+
 interface LocationRepository {
 
-    fun isLocationEnabled(): Boolean
+    fun observeIfLocationEnabledFlow(): Flow<Boolean>
     fun requestEnableLocation(onResult: (Boolean) -> Unit)
     suspend fun getLocation(): Pair<Double, Double>
 }

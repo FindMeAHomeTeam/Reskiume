6735dc74a684546821a640aa333e7211e7126831
juancoob <ja.cobos.obrero@gmail.com>
2026-02-05 16:59:19 +0100

    test: Create FakeCheckNonHumanAnimalUtil

---
+++ composeApp/src/commonTest/kotlin/com/findmeahometeam/reskiume/ui/integrationTests/fakes/FakeCheckNonHumanAnimalUtil.kt
@@ -0,0 +1,23 @@
+package com.findmeahometeam.reskiume.ui.integrationTests.fakes
+
+import com.findmeahometeam.reskiume.domain.model.NonHumanAnimal
+import com.findmeahometeam.reskiume.ui.core.components.UiState
+import com.findmeahometeam.reskiume.ui.profile.checkNonHumanAnimal.CheckNonHumanAnimalUtil
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flowOf
+
+class FakeCheckNonHumanAnimalUtil(
+    private val nonHumanAnimal: NonHumanAnimal = com.findmeahometeam.reskiume.nonHumanAnimal
+): CheckNonHumanAnimalUtil {
+
+    override fun getNonHumanAnimalFlow(
+        coroutineScope: CoroutineScope,
+        nonHumanAnimalId: String,
+        caregiverId: String
+    ): Flow<UiState<NonHumanAnimal>> = if(nonHumanAnimal.id == nonHumanAnimalId && nonHumanAnimal.caregiverId == caregiverId) {
+        flowOf(UiState.Success(nonHumanAnimal))
+    } else {
+        flowOf(UiState.Error("error getting non human animal"))
+    }
+}

3a021b0e10d09d99816f78453defb309fd552b9f
juancoob <ja.cobos.obrero@gmail.com>
2026-02-05 21:17:57 +0100

    refactor: Update CheckAllFosterHomesViewmodel with CheckAllMyFosterHomesUtil

--- composeApp/src/commonMain/kotlin/com/findmeahometeam/reskiume/ui/fosterHomes/checkAllFosterHomes/CheckAllFosterHomesViewmodel.kt
+++ composeApp/src/commonMain/kotlin/com/findmeahometeam/reskiume/ui/fosterHomes/checkAllFosterHomes/CheckAllFosterHomesViewmodel.kt
@@ -5,7 +5,6 @@ import androidx.lifecycle.viewModelScope
 import com.findmeahometeam.reskiume.data.remote.response.AuthUser
 import com.findmeahometeam.reskiume.data.util.Section
 import com.findmeahometeam.reskiume.data.util.log.Log
-import com.findmeahometeam.reskiume.domain.model.LocalCache
 import com.findmeahometeam.reskiume.domain.model.NonHumanAnimalType
 import com.findmeahometeam.reskiume.domain.model.fosterHome.City
 import com.findmeahometeam.reskiume.domain.model.fosterHome.Country
@@ -16,14 +15,8 @@ import com.findmeahometeam.reskiume.domain.usecases.fosterHome.GetAllFosterHomes
 import com.findmeahometeam.reskiume.domain.usecases.fosterHome.GetAllFosterHomesByCountryAndCityFromRemoteRepository
 import com.findmeahometeam.reskiume.domain.usecases.fosterHome.GetAllFosterHomesByLocationFromLocalRepository
 import com.findmeahometeam.reskiume.domain.usecases.fosterHome.GetAllFosterHomesByLocationFromRemoteRepository
-import com.findmeahometeam.reskiume.domain.usecases.fosterHome.GetFosterHomeFromLocalRepository
-import com.findmeahometeam.reskiume.domain.usecases.fosterHome.InsertFosterHomeInLocalRepository
-import com.findmeahometeam.reskiume.domain.usecases.fosterHome.ModifyFosterHomeInLocalRepository
-import com.findmeahometeam.reskiume.domain.usecases.image.DownloadImageToLocalDataSource
 import com.findmeahometeam.reskiume.domain.usecases.image.GetCompleteImagePathFromLocalDataSource
 import com.findmeahometeam.reskiume.domain.usecases.localCache.GetDataByManagingObjectLocalCacheTimestamp
-import com.findmeahometeam.reskiume.domain.usecases.localCache.InsertCacheInLocalRepository
-import com.findmeahometeam.reskiume.domain.usecases.localCache.ModifyCacheInLocalRepository
 import com.findmeahometeam.reskiume.domain.usecases.util.location.GetLocationFromLocationRepository
 import com.findmeahometeam.reskiume.domain.usecases.util.location.ObserveIfLocationEnabledFromLocationRepository
 import com.findmeahometeam.reskiume.domain.usecases.util.location.RequestEnableLocationFromLocationRepository
@@ -31,13 +24,13 @@ import com.findmeahometeam.reskiume.ui.core.components.UiState
 import com.findmeahometeam.reskiume.ui.core.components.UiState.Error
 import com.findmeahometeam.reskiume.ui.core.components.UiState.Idle
 import com.findmeahometeam.reskiume.ui.core.components.toUiState
+import com.findmeahometeam.reskiume.ui.profile.checkAllMyFosterHomes.CheckAllMyFosterHomesUtil
 import com.findmeahometeam.reskiume.ui.util.StringProvider
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.firstOrNull
 import kotlinx.coroutines.flow.flatMapConcat
 import kotlinx.coroutines.flow.flatMapLatest
 import kotlinx.coroutines.flow.flow
@@ -67,12 +60,7 @@ class CheckAllFosterHomesViewmodel(
     private val observeAuthStateInAuthDataSource: ObserveAuthStateInAuthDataSource,
     private val getDataByManagingObjectLocalCacheTimestamp: GetDataByManagingObjectLocalCacheTimestamp,
     private val getAllFosterHomesByCountryAndCityFromRemoteRepository: GetAllFosterHomesByCountryAndCityFromRemoteRepository,
-    private val downloadImageToLocalDataSource: DownloadImageToLocalDataSource,
-    private val getFosterHomeFromLocalRepository: GetFosterHomeFromLocalRepository,
-    private val insertFosterHomeInLocalRepository: InsertFosterHomeInLocalRepository,
-    private val insertCacheInLocalRepository: InsertCacheInLocalRepository,
-    private val modifyFosterHomeInLocalRepository: ModifyFosterHomeInLocalRepository,
-    private val modifyCacheInLocalRepository: ModifyCacheInLocalRepository,
+    private val checkAllMyFosterHomesUtil: CheckAllMyFosterHomesUtil,
     private val getAllFosterHomesByCountryAndCityFromLocalRepository: GetAllFosterHomesByCountryAndCityFromLocalRepository,
     private val getCompleteImagePathFromLocalDataSource: GetCompleteImagePathFromLocalDataSource,
     private val getAllFosterHomesByLocationFromRemoteRepository: GetAllFosterHomesByLocationFromRemoteRepository,
@@ -83,7 +71,7 @@ class CheckAllFosterHomesViewmodel(
     private val log: Log
 ) : ViewModel() {
 
-    private var myUserId: String = ""
+    private var myUid: String = ""
 
     private var activistLongitude: Double = 0.0
 
@@ -234,25 +222,35 @@ class CheckAllFosterHomesViewmodel(
         observeAuthStateInAuthDataSource()
             .flatMapConcat { authUser: AuthUser? ->
 
-                myUserId = authUser?.uid ?: " "
+                myUid = authUser?.uid ?: " "
 
                 getDataByManagingObjectLocalCacheTimestamp(
                     cachedObjectId = country + city,
-                    savedBy = myUserId,
+                    savedBy = myUid,
                     section = Section.FOSTER_HOMES,
                     onCompletionInsertCache = {
-                        getAllFosterHomesByCountryAndCityFromRemoteRepository(
+                        val allFosterHomesFlow: Flow<List<FosterHome>> = getAllFosterHomesByCountryAndCityFromRemoteRepository(
                             country,
                             city,
                             viewModelScope
-                        ).downloadImageAndManageFosterHomesInLocalRepository()
+                        )
+                        checkAllMyFosterHomesUtil.downloadImageAndManageFosterHomesInLocalRepositoryFromFlow(
+                            allFosterHomesFlow,
+                            viewModelScope,
+                            myUid
+                        )
                     },
                     onCompletionUpdateCache = {
-                        getAllFosterHomesByCountryAndCityFromRemoteRepository(
+                        val allFosterHomesFlow: Flow<List<FosterHome>> = getAllFosterHomesByCountryAndCityFromRemoteRepository(
                             country,
                             city,
                             viewModelScope
-                        ).downloadImageAndModifyFosterHomesInLocalRepository()
+                        )
+                        checkAllMyFosterHomesUtil.downloadImageAndModifyFosterHomesInLocalRepositoryFromFlow(
+                            allFosterHomesFlow,
+                            viewModelScope,
+                            myUid
+                        )
                     },
                     onVerifyCacheIsRecent = {
                         getAllFosterHomesByCountryAndCityFromLocalRepository(
@@ -286,170 +284,6 @@ class CheckAllFosterHomesViewmodel(
                 }
             }.toUiState()
 
-    private fun Flow<List<FosterHome>>.downloadImageAndManageFosterHomesInLocalRepository(): Flow<List<FosterHome>> =
-        this.map { fosterHomeList ->
-            fosterHomeList.map { fosterHome ->
-
-                if (fosterHome.imageUrl.isNotBlank()) {
-
-                    val localImagePath: String = downloadImageToLocalDataSource(
-                        userUid = fosterHome.ownerId,
-                        extraId = fosterHome.id,
-                        section = Section.FOSTER_HOMES
-                    )
-                    val fosterHomeWithLocalImage =
-                        fosterHome.copy(imageUrl = localImagePath.ifBlank { fosterHome.imageUrl })
-
-                    val localFosterHome: FosterHome? = getFosterHomeFromLocalRepository(
-                        fosterHome.id,
-                        viewModelScope
-                    ).firstOrNull()
-
-                    if (localFosterHome == null) {
-                        insertFosterHomeInLocalRepository(fosterHomeWithLocalImage)
-                    } else {
-                        modifyFosterHomeInLocalRepository(fosterHomeWithLocalImage)
-                    }
-                    fosterHomeWithLocalImage
-                } else {
-                    log.d(
-                        "CheckAllFosterHomesViewmodel",
-                        "Foster home ${fosterHome.id} has no avatar image to save locally."
-                    )
-                    val localFosterHome: FosterHome? = getFosterHomeFromLocalRepository(
-                        fosterHome.id,
-                        viewModelScope
-                    ).firstOrNull()
-
-                    if (localFosterHome == null) {
-                        insertFosterHomeInLocalRepository(fosterHome)
-                    } else {
-                        modifyFosterHomeInLocalRepository(fosterHome)
-                    }
-                    fosterHome
-                }
-            }
-        }
-
-    @OptIn(ExperimentalTime::class)
-    private fun insertFosterHomeInLocalRepository(fosterHome: FosterHome) {
-
-        viewModelScope.launch {
-
-            insertFosterHomeInLocalRepository(fosterHome) { isSuccess ->
-                if (isSuccess) {
-                    log.d(
-                        "CheckAllFosterHomesViewmodel",
-                        "Foster home ${fosterHome.id} added to local database"
-                    )
-                    viewModelScope.launch {
-
-                        insertCacheInLocalRepository(
-                            LocalCache(
-                                cachedObjectId = fosterHome.id,
-                                savedBy = myUserId,
-                                section = Section.FOSTER_HOMES,
-                                timestamp = Clock.System.now().epochSeconds
-                            )
-                        ) { rowId ->
-
-                            if (rowId > 0) {
-                                log.d(
-                                    "CheckAllFosterHomesViewmodel",
-                                    "${fosterHome.id} added to local cache in section ${Section.FOSTER_HOMES}"
-                                )
-                            } else {
-                                log.e(
-                                    "CheckAllFosterHomesViewmodel",
-                                    "Error adding ${fosterHome.id} to local cache in section ${Section.FOSTER_HOMES}"
-                                )
-                            }
-                        }
-                    }
-                } else {
-                    log.e(
-                        "CheckAllFosterHomesViewmodel",
-                        "Error adding the foster home ${fosterHome.id} to local database"
-                    )
-                }
-            }
-        }
-    }
-
-    @OptIn(ExperimentalTime::class)
-    private fun modifyFosterHomeInLocalRepository(fosterHome: FosterHome) {
-
-        viewModelScope.launch {
-
-            modifyFosterHomeInLocalRepository(fosterHome) { isSuccess ->
-                if (isSuccess) {
-                    log.d(
-                        "CheckAllFosterHomesViewmodel",
-                        "Foster home ${fosterHome.id} modified in local database"
-                    )
-                    viewModelScope.launch {
-
-                        modifyCacheInLocalRepository(
-                            LocalCache(
-                                cachedObjectId = fosterHome.id,
-                                savedBy = myUserId,
-                                section = Section.FOSTER_HOMES,
-                                timestamp = Clock.System.now().epochSeconds
-                            )
-                        ) { rowsUpdated ->
-
-                            if (rowsUpdated > 0) {
-                                log.d(
-                                    "CheckAllFosterHomesViewmodel",
-                                    "${fosterHome.id} updated in local cache in section ${Section.FOSTER_HOMES}"
-                                )
-                            } else {
-                                log.e(
-                                    "CheckAllFosterHomesViewmodel",
-                                    "Error updating ${fosterHome.id} in local cache in section ${Section.FOSTER_HOMES}"
-                                )
-                            }
-                        }
-                    }
-                } else {
-                    log.e(
-                        "CheckAllFosterHomesViewmodel",
-                        "Error modifying the foster home ${fosterHome.id} in local database"
-                    )
-                }
-            }
-        }
-    }
-
-    private fun Flow<List<FosterHome>>.downloadImageAndModifyFosterHomesInLocalRepository(): Flow<List<FosterHome>> =
-        this.map { fosterHomeList ->
-            fosterHomeList.map { fosterHome ->
-
-                if (fosterHome.imageUrl.isNotBlank()) {
-
-                    val localImagePath: String = downloadImageToLocalDataSource(
-                        userUid = fosterHome.ownerId,
-                        extraId = fosterHome.id,
-                        section = Section.FOSTER_HOMES
-                    )
-                    val fosterHomeWithLocalImage =
-                        fosterHome.copy(imageUrl = localImagePath.ifBlank { fosterHome.imageUrl })
-
-                    modifyFosterHomeInLocalRepository(fosterHomeWithLocalImage)
-
-                    fosterHomeWithLocalImage
-                } else {
-                    log.d(
-                        "CheckAllFosterHomesViewmodel",
-                        "Foster home ${fosterHome.id} has no avatar image to save locally."
-                    )
-                    modifyFosterHomeInLocalRepository(fosterHome)
-
-                    fosterHome
-                }
-            }
-        }
-
     private fun getFetchAllFosterHomesStateByLocationFlow(
         activistLongitude: Double,
         activistLatitude: Double,
@@ -458,29 +292,39 @@ class CheckAllFosterHomesViewmodel(
         observeAuthStateInAuthDataSource()
             .flatMapConcat { authUser: AuthUser? ->
 
-                myUserId = authUser?.uid ?: " "
+                myUid = authUser?.uid ?: " "
 
                 getDataByManagingObjectLocalCacheTimestamp(
                     cachedObjectId = "$activistLongitude$activistLatitude",
-                    savedBy = myUserId,
+                    savedBy = myUid,
                     section = Section.FOSTER_HOMES,
                     onCompletionInsertCache = {
-                        getAllFosterHomesByLocationFromRemoteRepository(
+                        val allFosterHomesFlow: Flow<List<FosterHome>> = getAllFosterHomesByLocationFromRemoteRepository(
                             activistLongitude = activistLongitude,
                             activistLatitude = activistLatitude,
                             rangeLongitude = getRangeLon(activistLatitude = activistLatitude),
                             rangeLatitude = getRangeLat(),
                             coroutineScope = viewModelScope
-                        ).downloadImageAndManageFosterHomesInLocalRepository()
+                        )
+                        checkAllMyFosterHomesUtil.downloadImageAndManageFosterHomesInLocalRepositoryFromFlow(
+                            allFosterHomesFlow,
+                            viewModelScope,
+                            myUid
+                        )
                     },
                     onCompletionUpdateCache = {
-                        getAllFosterHomesByLocationFromRemoteRepository(
+                        val allFosterHomesFlow: Flow<List<FosterHome>> = getAllFosterHomesByLocationFromRemoteRepository(
                             activistLongitude = activistLongitude,
                             activistLatitude = activistLatitude,
                             rangeLongitude = getRangeLon(activistLatitude = activistLatitude),
                             rangeLatitude = getRangeLat(),
                             coroutineScope = viewModelScope
-                        ).downloadImageAndModifyFosterHomesInLocalRepository()
+                        )
+                        checkAllMyFosterHomesUtil.downloadImageAndModifyFosterHomesInLocalRepositoryFromFlow(
+                            allFosterHomesFlow,
+                            viewModelScope,
+                            myUid
+                        )
                     },
                     onVerifyCacheIsRecent = {
                         getAllFosterHomesByLocationFromLocalRepository(

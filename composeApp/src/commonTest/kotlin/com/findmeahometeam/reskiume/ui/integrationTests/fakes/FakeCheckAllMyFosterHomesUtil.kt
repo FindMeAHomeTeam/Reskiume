435298175f4e3617dcf917a18103060a85ab42cf
juancoob <ja.cobos.obrero@gmail.com>
2026-02-06 11:30:03 +0100

    test: Create FakeCheckAllMyFosterHomesUtil

---
+++ composeApp/src/commonTest/kotlin/com/findmeahometeam/reskiume/ui/integrationTests/fakes/FakeCheckAllMyFosterHomesUtil.kt
@@ -0,0 +1,21 @@
+package com.findmeahometeam.reskiume.ui.integrationTests.fakes
+
+import com.findmeahometeam.reskiume.domain.model.fosterHome.FosterHome
+import com.findmeahometeam.reskiume.ui.profile.checkAllMyFosterHomes.CheckAllMyFosterHomesUtil
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.flow.Flow
+
+class FakeCheckAllMyFosterHomesUtil: CheckAllMyFosterHomesUtil {
+
+    override fun downloadImageAndManageFosterHomesInLocalRepositoryFromFlow(
+        allFosterHomesFlow: Flow<List<FosterHome>>,
+        coroutineScope: CoroutineScope,
+        myUid: String
+    ): Flow<List<FosterHome>> = allFosterHomesFlow
+
+    override fun downloadImageAndModifyFosterHomesInLocalRepositoryFromFlow(
+        allFosterHomesFlow: Flow<List<FosterHome>>,
+        coroutineScope: CoroutineScope,
+        myUid: String
+    ): Flow<List<FosterHome>> = allFosterHomesFlow
+}

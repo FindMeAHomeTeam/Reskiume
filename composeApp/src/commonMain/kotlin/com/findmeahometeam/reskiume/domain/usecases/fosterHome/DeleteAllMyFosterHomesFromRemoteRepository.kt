acfe6b4362b06717f38c9a283cd26db797a780c8
juancoob <ja.cobos.obrero@gmail.com>
2026-02-17 20:51:03 +0100

    refactor: Update DeleteAllMyFosterHomesFromRemoteRepository To check if the remote resident non human animal was deleted

--- composeApp/src/commonMain/kotlin/com/findmeahometeam/reskiume/domain/usecases/fosterHome/DeleteAllMyFosterHomesFromRemoteRepository.kt
+++ composeApp/src/commonMain/kotlin/com/findmeahometeam/reskiume/domain/usecases/fosterHome/DeleteAllMyFosterHomesFromRemoteRepository.kt
@@ -1,28 +1,37 @@
 package com.findmeahometeam.reskiume.domain.usecases.fosterHome
 
 import com.findmeahometeam.reskiume.data.remote.response.DatabaseResult
+import com.findmeahometeam.reskiume.data.remote.response.RemoteNonHumanAnimal
 import com.findmeahometeam.reskiume.data.remote.response.fosterHome.RemoteFosterHome
 import com.findmeahometeam.reskiume.data.util.log.Log
 import com.findmeahometeam.reskiume.domain.model.AdoptionState
 import com.findmeahometeam.reskiume.domain.repository.remote.database.remoteNonHumanAnimal.RealtimeDatabaseRemoteNonHumanAnimalRepository
 import com.findmeahometeam.reskiume.domain.repository.remote.fireStore.remoteFosterHome.FireStoreRemoteFosterHomeRepository
-import com.findmeahometeam.reskiume.ui.profile.checkNonHumanAnimal.CheckNonHumanAnimalUtil
+import com.findmeahometeam.reskiume.domain.usecases.authUser.ObserveAuthStateInAuthDataSource
+import com.findmeahometeam.reskiume.ui.profile.modifyNonHumanAnimal.DeleteNonHumanAnimalUtil
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.firstOrNull
 
 class DeleteAllMyFosterHomesFromRemoteRepository(
+    private val observeAuthStateInAuthDataSource: ObserveAuthStateInAuthDataSource,
     private val fireStoreRemoteFosterHomeRepository: FireStoreRemoteFosterHomeRepository,
-    private val checkNonHumanAnimalUtil: CheckNonHumanAnimalUtil,
+    private val deleteNonHumanAnimalUtil: DeleteNonHumanAnimalUtil,
     private val realtimeDatabaseRemoteNonHumanAnimalRepository: RealtimeDatabaseRemoteNonHumanAnimalRepository,
     private val log: Log
 ) {
 
     suspend operator fun invoke(
         ownerId: String,
+        coroutineScope: CoroutineScope,
         onDeleteAllMyRemoteFosterHomes: (result: DatabaseResult) -> Unit
     ) {
-        val isSuccess = updateAdoptionStates(ownerId)
-
+        val myUid = observeAuthStateInAuthDataSource().first()!!.uid
+        val isSuccess = manageResidents(
+            ownerId,
+            myUid,
+            coroutineScope
+        )
         if (isSuccess) {
             fireStoreRemoteFosterHomeRepository.deleteAllMyRemoteFosterHomes(
                 ownerId,
@@ -31,43 +40,64 @@ class DeleteAllMyFosterHomesFromRemoteRepository(
         }
     }
 
-    private suspend fun updateAdoptionStates(ownerId: String): Boolean {
+    private suspend fun manageResidents(
+        ownerId: String,
+        myUid: String,
+        coroutineScope: CoroutineScope
+    ): Boolean {
         var isSuccess = true
 
         val remoteFosterHomes: List<RemoteFosterHome?> =
             fireStoreRemoteFosterHomeRepository.getAllMyRemoteFosterHomes(ownerId).first()
 
-        remoteFosterHomes.forEach { fosterHome ->
+        remoteFosterHomes.forEach { remoteFosterHome ->
+            remoteFosterHome!!.allResidentNonHumanAnimalIds!!.forEach { residentNonHumanAnimalForFosterHome ->
 
-            fosterHome!!.allResidentNonHumanAnimalIds!!.forEach { residentNonHumanAnimalForFosterHome ->
                 if (isSuccess) {
-                    val residentNonHumanAnimalForFosterHome =
-                        residentNonHumanAnimalForFosterHome.toDomain(
-                            onFetchNonHumanAnimal = { nonHumanAnimalId: String, caregiverId: String ->
+                    val remoteResidentNonHumanAnimal: RemoteNonHumanAnimal? =
+                        realtimeDatabaseRemoteNonHumanAnimalRepository.getRemoteNonHumanAnimal(
+                            residentNonHumanAnimalForFosterHome.nonHumanAnimalId!!,
+                            residentNonHumanAnimalForFosterHome.caregiverId!!,
+                        ).firstOrNull()
 
-                                checkNonHumanAnimalUtil.getNonHumanAnimalFlow(
-                                    nonHumanAnimalId = nonHumanAnimalId,
-                                    caregiverId = caregiverId
-                                ).firstOrNull()
+                    if (remoteResidentNonHumanAnimal == null) {
+                        deleteNonHumanAnimalUtil.deleteNonHumanAnimal(
+                            id = residentNonHumanAnimalForFosterHome.nonHumanAnimalId,
+                            caregiverId = residentNonHumanAnimalForFosterHome.caregiverId,
+                            coroutineScope = coroutineScope,
+                            onlyDeleteOnLocal = myUid != residentNonHumanAnimalForFosterHome.caregiverId,
+                            onError = {
+                                log.e(
+                                    "DeleteAllMyFosterHomesFromRemoteRepository",
+                                    "manageResidents: Can not delete the non human animal ${residentNonHumanAnimalForFosterHome.nonHumanAnimalId} in the local data source"
+                                )
+                            },
+                            onComplete = {
+                                log.d(
+                                    "DeleteAllMyFosterHomesFromRemoteRepository",
+                                    "manageResidents: Deleted the non human animal ${residentNonHumanAnimalForFosterHome.nonHumanAnimalId} in the local data source"
+                                )
                             }
                         )
-                    realtimeDatabaseRemoteNonHumanAnimalRepository.modifyRemoteNonHumanAnimal(
-                        residentNonHumanAnimalForFosterHome.residentNonHumanAnimal!!.copy(
-                            adoptionState = AdoptionState.LOOKING_FOR_ADOPTION,
-                            fosterHomeId = ""
-                        ).toData()
-                    ) { databaseResult ->
-                        if (databaseResult is DatabaseResult.Success) {
-                            log.d(
-                                "DeleteAllMyFosterHomesFromRemoteRepository",
-                                "updateAdoptionStates: updated adoption state for the non human animal ${residentNonHumanAnimalForFosterHome.residentNonHumanAnimal.id} to ${AdoptionState.LOOKING_FOR_ADOPTION} in the remote data source"
+                    } else {
+                        realtimeDatabaseRemoteNonHumanAnimalRepository.modifyRemoteNonHumanAnimal(
+                            remoteResidentNonHumanAnimal.copy(
+                                adoptionState = AdoptionState.LOOKING_FOR_ADOPTION,
+                                fosterHomeId = ""
                             )
-                        } else {
-                            log.e(
-                                "DeleteAllMyFosterHomesFromRemoteRepository",
-                                "updateAdoptionStates: failed to update the adoption state for the non human animal ${residentNonHumanAnimalForFosterHome.residentNonHumanAnimal.id} in the remote data source"
-                            )
-                            isSuccess = false
+                        ) { databaseResult ->
+                            if (databaseResult is DatabaseResult.Success) {
+                                log.d(
+                                    "DeleteAllMyFosterHomesFromRemoteRepository",
+                                    "manageResidents: updated adoption state for the non human animal ${remoteResidentNonHumanAnimal.id} in the remote data source"
+                                )
+                            } else {
+                                log.e(
+                                    "DeleteAllMyFosterHomesFromRemoteRepository",
+                                    "manageResidents: failed to update the adoption state for the non human animal ${remoteResidentNonHumanAnimal.id} in the remote data source"
+                                )
+                                isSuccess = false
+                            }
                         }
                     }
                 }

9fc532cac3a1f4cbf19671c90bc637e012a493a7
juancoob <ja.cobos.obrero@gmail.com>
2026-02-22 14:03:37 +0100

    test: Update GetAllNonHumanAnimalsFromLocalRepositoryTest use case

--- composeApp/src/commonTest/kotlin/com/findmeahometeam/reskiume/usecases/nonHumanAnimal/GetAllNonHumanAnimalsFromLocalRepositoryTest.kt
+++ composeApp/src/commonTest/kotlin/com/findmeahometeam/reskiume/usecases/nonHumanAnimal/GetAllNonHumanAnimalsFromLocalRepositoryTest.kt
@@ -3,9 +3,8 @@ package com.findmeahometeam.reskiume.usecases.nonHumanAnimal
 import app.cash.turbine.test
 import com.findmeahometeam.reskiume.CoroutineTestDispatcher
 import com.findmeahometeam.reskiume.domain.repository.local.LocalNonHumanAnimalRepository
-import com.findmeahometeam.reskiume.domain.usecases.nonHumanAnimal.GetAllMyNonHumanAnimalsFromLocalRepository
+import com.findmeahometeam.reskiume.domain.usecases.nonHumanAnimal.GetAllNonHumanAnimalsFromLocalRepository
 import com.findmeahometeam.reskiume.nonHumanAnimal
-import com.findmeahometeam.reskiume.user
 import dev.mokkery.answering.returns
 import dev.mokkery.every
 import dev.mokkery.mock
@@ -18,17 +17,17 @@ class GetAllNonHumanAnimalsFromLocalRepositoryTest: CoroutineTestDispatcher() {
 
     val localNonHumanAnimalRepository: LocalNonHumanAnimalRepository = mock {
         every {
-            getAllMyNonHumanAnimals(user.uid)
+            getAllNonHumanAnimals()
         } returns flowOf(listOf(nonHumanAnimal.toEntity()))
     }
 
-    private val getAllMyNonHumanAnimalsFromLocalRepository =
-        GetAllMyNonHumanAnimalsFromLocalRepository(localNonHumanAnimalRepository)
+    private val getAllNonHumanAnimalsFromLocalRepository =
+        GetAllNonHumanAnimalsFromLocalRepository(localNonHumanAnimalRepository)
 
     @Test
     fun `given local non human animals_when the app retrieves them to list them_then app gets a flow of list of NonHumanAnimal`() =
         runTest {
-            getAllMyNonHumanAnimalsFromLocalRepository(user.uid).test {
+            getAllNonHumanAnimalsFromLocalRepository().test {
                 assertEquals(listOf(nonHumanAnimal), awaitItem())
                 awaitComplete()
             }

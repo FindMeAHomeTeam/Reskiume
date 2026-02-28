9c0c0788362344d5f094b8e8ccca3729c3ecfd21
juancoob <ja.cobos.obrero@gmail.com>
2026-02-28 13:10:01 +0100

    refactor: Update RmTextLink to add more properties to the text

--- composeApp/src/commonMain/kotlin/com/findmeahometeam/reskiume/ui/core/components/RmTextLink.kt
+++ composeApp/src/commonMain/kotlin/com/findmeahometeam/reskiume/ui/core/components/RmTextLink.kt
@@ -3,12 +3,17 @@ package com.findmeahometeam.reskiume.ui.core.components
 import androidx.compose.foundation.layout.Row
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.LinkAnnotation
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.TextLinkStyles
 import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.sp
 import com.findmeahometeam.reskiume.ui.core.primaryGreen
 import com.findmeahometeam.reskiume.ui.core.textColor
 
@@ -16,6 +21,10 @@ import com.findmeahometeam.reskiume.ui.core.textColor
 fun RmTextLink(
     text: String,
     textToLink: String,
+    modifier: Modifier = Modifier,
+    textAlign: TextAlign = TextAlign.Start,
+    fontWeight: FontWeight = FontWeight.Normal,
+    fontSize: TextUnit = 14.sp,
     onClick: () -> Unit
 ) {
     val startIndex = text.indexOf(textToLink)
@@ -45,7 +54,11 @@ fun RmTextLink(
     }
     Row {
         Text(
+            modifier = modifier,
             text = annotatedLinkString,
+            textAlign = textAlign,
+            fontWeight = fontWeight,
+            fontSize = fontSize,
             color = textColor
         )
     }

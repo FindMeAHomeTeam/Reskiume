845fc65df706674b08d76be23127c583a45ec906
juancoob <ja.cobos.obrero@gmail.com>
2026-03-03 12:40:28 +0100

    refactor: Update the preview import on App

--- composeApp/src/commonMain/kotlin/com/findmeahometeam/reskiume/App.kt
+++ composeApp/src/commonMain/kotlin/com/findmeahometeam/reskiume/App.kt
@@ -1,12 +1,12 @@
 package com.findmeahometeam.reskiume
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
 import com.findmeahometeam.reskiume.di.dataModule
 import com.findmeahometeam.reskiume.di.domainModule
 import com.findmeahometeam.reskiume.di.platformModule
 import com.findmeahometeam.reskiume.di.uiModule
 import com.findmeahometeam.reskiume.ui.core.navigation.NavigationWrapper
-import org.jetbrains.compose.ui.tooling.preview.Preview
 import org.koin.compose.KoinApplication
 import org.koin.dsl.KoinAppDeclaration
 

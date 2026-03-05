ccbd0ede22e2b4fbf5aac796a77668996cb04c55
juancoob <ja.cobos.obrero@gmail.com>
2026-03-05 13:16:56 +0100

    test: Create FakeCheckActivistUtil

---
+++ composeApp/src/commonTest/kotlin/com/findmeahometeam/reskiume/ui/integrationTests/fakes/FakeCheckActivistUtil.kt
@@ -0,0 +1,21 @@
+package com.findmeahometeam.reskiume.ui.integrationTests.fakes
+
+import com.findmeahometeam.reskiume.domain.model.User
+import com.findmeahometeam.reskiume.ui.profile.checkReviews.CheckActivistUtil
+import com.findmeahometeam.reskiume.user
+
+class FakeCheckActivistUtil(
+    private val activistToCheck: User? = user
+): CheckActivistUtil {
+
+    override suspend fun getUser(
+        activistUid: String,
+        myUserUid: String
+    ): User? {
+        return if (activistUid == activistToCheck?.uid) {
+            user
+        } else {
+            null
+        }
+    }
+}

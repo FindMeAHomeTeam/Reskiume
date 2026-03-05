3ad5bf44ae714e71cb8497865a8e4f2756a42cf4
juancoob <ja.cobos.obrero@gmail.com>
2026-03-05 13:15:50 +0100

    test: Update naming in FakeCheckNonHumanAnimalUtil

--- composeApp/src/commonTest/kotlin/com/findmeahometeam/reskiume/ui/integrationTests/fakes/FakeCheckNonHumanAnimalUtil.kt
+++ composeApp/src/commonTest/kotlin/com/findmeahometeam/reskiume/ui/integrationTests/fakes/FakeCheckNonHumanAnimalUtil.kt
@@ -1,21 +1,22 @@
 package com.findmeahometeam.reskiume.ui.integrationTests.fakes
 
 import com.findmeahometeam.reskiume.domain.model.NonHumanAnimal
+import com.findmeahometeam.reskiume.nonHumanAnimal
 import com.findmeahometeam.reskiume.ui.profile.checkNonHumanAnimal.CheckNonHumanAnimalUtil
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flowOf
 
 class FakeCheckNonHumanAnimalUtil(
-    private val nonHumanAnimal: NonHumanAnimal = com.findmeahometeam.reskiume.nonHumanAnimal
+    private val nonHumanAnimalToReview: NonHumanAnimal = nonHumanAnimal
 ): CheckNonHumanAnimalUtil {
 
     override fun getNonHumanAnimalFlow(
         nonHumanAnimalId: String,
         caregiverId: String,
         coroutineScope: CoroutineScope
-    ): Flow<NonHumanAnimal> = if(nonHumanAnimal.id == nonHumanAnimalId && nonHumanAnimal.caregiverId == caregiverId) {
-        flowOf(nonHumanAnimal)
+    ): Flow<NonHumanAnimal> = if(nonHumanAnimalToReview.id == nonHumanAnimalId && nonHumanAnimalToReview.caregiverId == caregiverId) {
+        flowOf(nonHumanAnimalToReview)
     } else {
         flowOf()
     }

117f4908169969dfc9a64e0da0f67f354edd4d58
juancoob <ja.cobos.obrero@gmail.com>
2026-03-08 18:23:52 +0100

    test: Add GetAllFosterHomesFromLocalRepositoryTest

---
+++ composeApp/src/commonTest/kotlin/com/findmeahometeam/reskiume/usecases/fosterHome/GetAllFosterHomesFromLocalRepositoryTest.kt
@@ -0,0 +1,36 @@
+package com.findmeahometeam.reskiume.usecases.fosterHome
+
+import app.cash.turbine.test
+import com.findmeahometeam.reskiume.CoroutineTestDispatcher
+import com.findmeahometeam.reskiume.domain.repository.local.LocalFosterHomeRepository
+import com.findmeahometeam.reskiume.domain.usecases.fosterHome.GetAllFosterHomesFromLocalRepository
+import com.findmeahometeam.reskiume.fosterHome
+import com.findmeahometeam.reskiume.fosterHomeWithAllNonHumanAnimalData
+import dev.mokkery.answering.returns
+import dev.mokkery.everySuspend
+import dev.mokkery.mock
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.test.runTest
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class GetAllFosterHomesFromLocalRepositoryTest: CoroutineTestDispatcher() {
+
+    private val localFosterHomeRepository: LocalFosterHomeRepository = mock {
+        everySuspend {
+            getAllFosterHomes()
+        } returns flowOf(listOf(fosterHomeWithAllNonHumanAnimalData))
+    }
+
+    private val getAllFosterHomesFromLocalRepository =
+        GetAllFosterHomesFromLocalRepository(localFosterHomeRepository)
+
+    @Test
+    fun `given local foster homes_when the app retrieves them to list them_then app gets a flow of list of FosterHome`() =
+        runTest {
+            getAllFosterHomesFromLocalRepository().test {
+                assertEquals(listOf(fosterHome), awaitItem())
+                awaitComplete()
+            }
+        }
+}

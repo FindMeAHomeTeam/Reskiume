be7915d8a06425636aba223690e3151f2c197a3f
juancoob <ja.cobos.obrero@gmail.com>
2026-03-12 12:27:53 +0100

    feat: Create ModifyRescueEventInRemoteRepository and its test

---
+++ composeApp/src/commonMain/kotlin/com/findmeahometeam/reskiume/domain/usecases/rescueEvent/ModifyRescueEventInRemoteRepository.kt
@@ -0,0 +1,155 @@
+package com.findmeahometeam.reskiume.domain.usecases.rescueEvent
+
+import com.findmeahometeam.reskiume.data.remote.response.DatabaseResult
+import com.findmeahometeam.reskiume.data.remote.response.RemoteNonHumanAnimal
+import com.findmeahometeam.reskiume.data.util.log.Log
+import com.findmeahometeam.reskiume.domain.model.AdoptionState
+import com.findmeahometeam.reskiume.domain.model.rescueEvent.NonHumanAnimalToRescue
+import com.findmeahometeam.reskiume.domain.model.rescueEvent.RescueEvent
+import com.findmeahometeam.reskiume.domain.repository.remote.auth.AuthRepository
+import com.findmeahometeam.reskiume.domain.repository.remote.database.remoteNonHumanAnimal.RealtimeDatabaseRemoteNonHumanAnimalRepository
+import com.findmeahometeam.reskiume.domain.repository.remote.fireStore.remoteRescueEvent.FireStoreRemoteRescueEventRepository
+import com.findmeahometeam.reskiume.ui.profile.modifyNonHumanAnimal.DeleteNonHumanAnimalUtil
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.firstOrNull
+
+class ModifyRescueEventInRemoteRepository(
+    private val authRepository: AuthRepository,
+    private val fireStoreRemoteRescueEventRepository: FireStoreRemoteRescueEventRepository,
+    private val realtimeDatabaseRemoteNonHumanAnimalRepository: RealtimeDatabaseRemoteNonHumanAnimalRepository,
+    private val deleteNonHumanAnimalUtil: DeleteNonHumanAnimalUtil,
+    private val log: Log
+) {
+    suspend operator fun invoke(
+        updatedRescueEvent: RescueEvent,
+        coroutineScope: CoroutineScope,
+        onModifyRemoteRescueEvent: (result: DatabaseResult) -> Unit
+    ) {
+        val myUid = authRepository.authState.first()!!.uid
+
+        val previousRescueEvent = fireStoreRemoteRescueEventRepository.getRemoteRescueEvent(
+            updatedRescueEvent.id
+        ).firstOrNull()?.toDomain() ?: return
+
+        val isSuccess = manageAllNonHumanAnimalsToRescue(
+            myUid = myUid,
+            previousRescueEvent = previousRescueEvent,
+            updatedRescueEvent = updatedRescueEvent,
+            coroutineScope = coroutineScope
+        )
+        if (isSuccess) {
+            fireStoreRemoteRescueEventRepository.modifyRemoteRescueEvent(
+                updatedRescueEvent.toData(),
+                onModifyRemoteRescueEvent
+            )
+        }
+    }
+
+    private suspend fun manageAllNonHumanAnimalsToRescue(
+        myUid: String,
+        previousRescueEvent: RescueEvent,
+        updatedRescueEvent: RescueEvent,
+        coroutineScope: CoroutineScope
+    ): Boolean {
+        val previousAllNonHumanAnimalsToRescue =
+            previousRescueEvent.allNonHumanAnimalsToRescue.toSet()
+
+        val updatedAllNonHumanAnimalsToRescue =
+            updatedRescueEvent.allNonHumanAnimalsToRescue.toSet()
+
+        val nonHumanAnimalsToRescueToManage: Set<NonHumanAnimalToRescue> =
+            (previousAllNonHumanAnimalsToRescue - updatedAllNonHumanAnimalsToRescue) +
+                    (updatedAllNonHumanAnimalsToRescue - previousAllNonHumanAnimalsToRescue)
+
+        println("size: ${nonHumanAnimalsToRescueToManage.size}")
+
+        nonHumanAnimalsToRescueToManage.forEach { nonHumanAnimalToRescue ->
+
+            val remoteNonHumanAnimal: RemoteNonHumanAnimal =
+                realtimeDatabaseRemoteNonHumanAnimalRepository.getRemoteNonHumanAnimal(
+                    nonHumanAnimalToRescue.nonHumanAnimalId,
+                    nonHumanAnimalToRescue.caregiverId,
+                ).firstOrNull() ?: return false.also {
+                    deleteNonHumanAnimal(
+                        nonHumanAnimalToRescue,
+                        myUid,
+                        coroutineScope
+                    )
+                }
+
+            val containsNonHumanAnimalToRescue =
+                updatedAllNonHumanAnimalsToRescue.contains(nonHumanAnimalToRescue)
+            updateAdoptionState(
+                remoteNonHumanAnimal,
+                containsNonHumanAnimalToRescue
+            )
+
+        }
+        return true
+    }
+
+    private fun deleteNonHumanAnimal(
+        nonHumanAnimalToRescue: NonHumanAnimalToRescue,
+        myUid: String,
+        coroutineScope: CoroutineScope
+    ) {
+        log.d(
+            "ModifyRescueEventInRemoteRepository",
+            "deleteNonHumanAnimal: Can not update the adoption state for the non human animal in the rescue event ${nonHumanAnimalToRescue.rescueEventId} because the non human animal has been unregistered in the remote data source"
+        )
+
+        deleteNonHumanAnimalUtil.deleteNonHumanAnimal(
+            id = nonHumanAnimalToRescue.nonHumanAnimalId,
+            caregiverId = nonHumanAnimalToRescue.caregiverId,
+            coroutineScope = coroutineScope,
+            onlyDeleteOnLocal = myUid != nonHumanAnimalToRescue.caregiverId,
+            onError = {
+                log.e(
+                    "ModifyRescueEventInRemoteRepository",
+                    "deleteNonHumanAnimal: Can not delete the non human animal ${nonHumanAnimalToRescue.nonHumanAnimalId} in the local data source"
+                )
+            },
+            onComplete = {
+                log.d(
+                    "ModifyRescueEventInRemoteRepository",
+                    "deleteNonHumanAnimal: Deleted the non human animal ${nonHumanAnimalToRescue.nonHumanAnimalId} in the local data source"
+                )
+            }
+        )
+    }
+
+    private suspend fun updateAdoptionState(
+        remoteNonHumanAnimal: RemoteNonHumanAnimal,
+        containsNonHumanAnimalToRescue: Boolean
+    ) {
+        val adoptionState = when {
+            containsNonHumanAnimalToRescue -> {
+                AdoptionState.NEEDS_TO_BE_RESCUED
+            }
+            remoteNonHumanAnimal.adoptionState != AdoptionState.ADOPTED -> {
+                AdoptionState.LOOKING_FOR_ADOPTION
+            }
+            else -> return // is adopted
+        }
+        realtimeDatabaseRemoteNonHumanAnimalRepository.modifyRemoteNonHumanAnimal(
+            remoteNonHumanAnimal.copy(
+                adoptionState = adoptionState,
+                fosterHomeId = ""
+            )
+        ) { databaseResult ->
+
+            if (databaseResult is DatabaseResult.Success) {
+                log.d(
+                    "ModifyRescueEventInRemoteRepository",
+                    "updateAdoptionState: updated adoption state $adoptionState for the non human animal ${remoteNonHumanAnimal.id} in the remote data source"
+                )
+            } else {
+                log.e(
+                    "ModifyRescueEventInRemoteRepository",
+                    "updateAdoptionState: failed to update the adoption state $adoptionState for the non human animal ${remoteNonHumanAnimal.id} in the remote data source"
+                )
+            }
+        }
+    }
+}
---
+++ composeApp/src/commonTest/kotlin/com/findmeahometeam/reskiume/usecases/rescueEvent/ModifyRescueEventInRemoteRepositoryTest.kt
@@ -0,0 +1,266 @@
+package com.findmeahometeam.reskiume.usecases.rescueEvent
+
+import com.findmeahometeam.reskiume.authUser
+import com.findmeahometeam.reskiume.data.util.log.Log
+import com.findmeahometeam.reskiume.domain.model.AdoptionState
+import com.findmeahometeam.reskiume.domain.repository.remote.auth.AuthRepository
+import com.findmeahometeam.reskiume.domain.repository.remote.database.remoteNonHumanAnimal.RealtimeDatabaseRemoteNonHumanAnimalRepository
+import com.findmeahometeam.reskiume.domain.repository.remote.fireStore.remoteRescueEvent.FireStoreRemoteRescueEventRepository
+import com.findmeahometeam.reskiume.domain.usecases.rescueEvent.ModifyRescueEventInRemoteRepository
+import com.findmeahometeam.reskiume.nonHumanAnimal
+import com.findmeahometeam.reskiume.rescueEvent
+import com.findmeahometeam.reskiume.ui.profile.modifyNonHumanAnimal.DeleteNonHumanAnimalUtil
+import dev.mokkery.answering.calls
+import dev.mokkery.answering.returns
+import dev.mokkery.every
+import dev.mokkery.everySuspend
+import dev.mokkery.matcher.any
+import dev.mokkery.mock
+import dev.mokkery.verify.VerifyMode.Companion.exactly
+import dev.mokkery.verifySuspend
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.test.runTest
+import kotlin.test.Test
+
+class ModifyRescueEventInRemoteRepositoryTest {
+
+    val authRepository: AuthRepository = mock {
+        everySuspend { authState } returns flowOf(authUser)
+    }
+
+    private val fireStoreRemoteRescueEventRepository: FireStoreRemoteRescueEventRepository = mock {
+        everySuspend {
+            getRemoteRescueEvent(rescueEvent.id)
+        } returns flowOf(
+            rescueEvent.copy(
+                allNonHumanAnimalsToRescue = listOf(rescueEvent.allNonHumanAnimalsToRescue[0]),
+                allNeedsToCover = emptyList()
+            ).toData()
+        )
+
+        everySuspend {
+            getRemoteRescueEvent(rescueEvent.id + "rescued")
+        } returns flowOf(
+            rescueEvent.copy(
+                allNonHumanAnimalsToRescue = listOf(
+                    rescueEvent.allNonHumanAnimalsToRescue[0].copy(
+                        nonHumanAnimalId = nonHumanAnimal.id + "rescued"
+                    )
+                )
+            ).toData()
+        )
+
+        everySuspend {
+            getRemoteRescueEvent("otherRescueEventId")
+        } returns flowOf(
+            rescueEvent.copy(
+                allNonHumanAnimalsToRescue = listOf(rescueEvent.allNonHumanAnimalsToRescue[0]),
+                allNeedsToCover = emptyList()
+            ).toData()
+        )
+
+        everySuspend {
+            getRemoteRescueEvent("wrongId")
+        } returns flowOf()
+
+        everySuspend {
+            modifyRemoteRescueEvent(rescueEvent.toData(), any())
+        } returns Unit
+
+        everySuspend {
+            modifyRemoteRescueEvent(
+                rescueEvent.copy(
+                    id = rescueEvent.id + "rescued",
+                    allNonHumanAnimalsToRescue = emptyList(),
+                    allNeedsToCover = emptyList()
+                ).toData(),
+                any()
+            )
+        } returns Unit
+
+        everySuspend {
+            modifyRemoteRescueEvent(
+                rescueEvent.copy(
+                    id = "otherRescueEventId",
+                    allNonHumanAnimalsToRescue = rescueEvent.allNonHumanAnimalsToRescue.map {
+                        it.copy(nonHumanAnimalId = nonHumanAnimal.id + "other")
+                    }
+                ).toData(),
+                any()
+            )
+        } returns Unit
+    }
+
+    val realtimeDatabaseRemoteNonHumanAnimalRepository: RealtimeDatabaseRemoteNonHumanAnimalRepository =
+        mock {
+            everySuspend {
+                getRemoteNonHumanAnimal(nonHumanAnimal.id, nonHumanAnimal.caregiverId)
+            } returns flowOf(nonHumanAnimal.toData())
+
+            everySuspend {
+                getRemoteNonHumanAnimal(nonHumanAnimal.id + "second", nonHumanAnimal.caregiverId)
+            } returns flowOf(nonHumanAnimal.toData())
+
+            everySuspend {
+                getRemoteNonHumanAnimal(nonHumanAnimal.id + "other", nonHumanAnimal.caregiverId)
+            } returns flowOf(null)
+
+            everySuspend {
+                getRemoteNonHumanAnimal(nonHumanAnimal.id + "rescued", nonHumanAnimal.caregiverId)
+            } returns flowOf(nonHumanAnimal.copy(adoptionState = AdoptionState.ADOPTED).toData())
+
+            everySuspend {
+                modifyRemoteNonHumanAnimal(
+                    nonHumanAnimal.copy(
+                        adoptionState = AdoptionState.NEEDS_TO_BE_RESCUED
+                    ).toData(),
+                    any()
+                )
+            } returns Unit
+
+            everySuspend {
+                modifyRemoteNonHumanAnimal(
+                    nonHumanAnimal.copy(
+                        adoptionState = AdoptionState.LOOKING_FOR_ADOPTION
+                    ).toData(),
+                    any()
+                )
+            } returns Unit
+        }
+
+    val deleteNonHumanAnimalUtil: DeleteNonHumanAnimalUtil = mock {
+        everySuspend {
+            deleteNonHumanAnimal(
+                id = nonHumanAnimal.id + "other",
+                caregiverId = nonHumanAnimal.caregiverId,
+                coroutineScope = any(),
+                onlyDeleteOnLocal = false,
+                onError = any(),
+                onComplete = any()
+            )
+        } returns Unit
+    }
+
+    private val log: Log = mock {
+        every { d(any(), any()) } calls { println(it) }
+        every { e(any(), any()) } calls { println(it) }
+    }
+
+    private val modifyRescueEventInRemoteRepository =
+        ModifyRescueEventInRemoteRepository(
+            authRepository,
+            fireStoreRemoteRescueEventRepository,
+            realtimeDatabaseRemoteNonHumanAnimalRepository,
+            deleteNonHumanAnimalUtil,
+            log
+        )
+
+    @Test
+    fun `given a remote rescue event_when the app modifies it_then modifyRemoteNonHumanAnimal and modifyRemoteRescueEvent are called`() =
+        runTest {
+            modifyRescueEventInRemoteRepository(
+                rescueEvent,
+                coroutineScope = this
+            ) {}
+            verifySuspend {
+                realtimeDatabaseRemoteNonHumanAnimalRepository.modifyRemoteNonHumanAnimal(
+                    nonHumanAnimal.copy(
+                        adoptionState = AdoptionState.NEEDS_TO_BE_RESCUED
+                    ).toData(),
+                    any()
+                )
+                fireStoreRemoteRescueEventRepository.modifyRemoteRescueEvent(
+                    rescueEvent.toData(),
+                    any()
+                )
+            }
+        }
+
+    @Test
+    fun `given a remote rescue event_when the app try to modify it but fails retrieving the previous rescue event_then it will not call anything`() =
+        runTest {
+            modifyRescueEventInRemoteRepository(
+                rescueEvent.copy(id = "wrongId"),
+                coroutineScope = this
+            ) {}
+            verifySuspend(exactly(0)) {
+                realtimeDatabaseRemoteNonHumanAnimalRepository.modifyRemoteNonHumanAnimal(
+                    nonHumanAnimal.copy(
+                        adoptionState = AdoptionState.NEEDS_TO_BE_RESCUED
+                    ).toData(),
+                    any()
+                )
+                fireStoreRemoteRescueEventRepository.modifyRemoteRescueEvent(
+                    rescueEvent.toData(),
+                    any()
+                )
+            }
+        }
+
+    @Test
+    fun `given a remote rescue event_when the app modifies it but the resident was deleted_then only deleteNonHumanAnimal is called`() =
+        runTest {
+            modifyRescueEventInRemoteRepository(
+                rescueEvent.copy(
+                    id = "otherRescueEventId",
+                    allNonHumanAnimalsToRescue = rescueEvent.allNonHumanAnimalsToRescue.map {
+                        it.copy(nonHumanAnimalId = nonHumanAnimal.id + "other")
+                    }
+                ),
+                coroutineScope = this
+            ) {}
+            verifySuspend {
+                deleteNonHumanAnimalUtil.deleteNonHumanAnimal(
+                    id = nonHumanAnimal.id + "other",
+                    caregiverId = nonHumanAnimal.caregiverId,
+                    coroutineScope = any(),
+                    onlyDeleteOnLocal = false,
+                    onError = any(),
+                    onComplete = any()
+                )
+            }
+            verifySuspend(exactly(0)) {
+                realtimeDatabaseRemoteNonHumanAnimalRepository.modifyRemoteNonHumanAnimal(
+                    nonHumanAnimal.copy(
+                        adoptionState = AdoptionState.NEEDS_TO_BE_RESCUED
+                    ).toData(),
+                    any()
+                )
+                fireStoreRemoteRescueEventRepository.modifyRemoteRescueEvent(
+                    rescueEvent.toData(),
+                    any()
+                )
+            }
+        }
+
+    @Test
+    fun `given a remote rescue event_when the app modifies it but the non human animal was adopted_then the app only calls to modifyRemoteRescueEvent`() =
+        runTest {
+            modifyRescueEventInRemoteRepository(
+                rescueEvent.copy(
+                    id = rescueEvent.id + "rescued",
+                    allNonHumanAnimalsToRescue = emptyList(),
+                    allNeedsToCover = emptyList()
+                ),
+                coroutineScope = this
+            ) {}
+            verifySuspend {
+                fireStoreRemoteRescueEventRepository.modifyRemoteRescueEvent(
+                    rescueEvent.copy(
+                        id = rescueEvent.id + "rescued",
+                        allNonHumanAnimalsToRescue = emptyList(),
+                        allNeedsToCover = emptyList()
+                    ).toData(),
+                    any()
+                )
+            }
+            verifySuspend(exactly(0)) {
+                realtimeDatabaseRemoteNonHumanAnimalRepository.modifyRemoteNonHumanAnimal(
+                    nonHumanAnimal.copy(
+                        adoptionState = AdoptionState.NEEDS_TO_BE_RESCUED
+                    ).toData(),
+                    any()
+                )
+            }
+        }
+}

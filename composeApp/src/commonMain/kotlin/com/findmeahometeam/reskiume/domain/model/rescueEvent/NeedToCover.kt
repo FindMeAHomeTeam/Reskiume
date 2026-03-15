6bd829f7386fb83c740078b568153704085db237
juancoob <ja.cobos.obrero@gmail.com>
2026-03-15 19:02:32 +0100

    feat: Add need to cover strings

--- composeApp/src/commonMain/kotlin/com/findmeahometeam/reskiume/domain/model/rescueEvent/NeedToCover.kt
+++ composeApp/src/commonMain/kotlin/com/findmeahometeam/reskiume/domain/model/rescueEvent/NeedToCover.kt
@@ -2,6 +2,11 @@ package com.findmeahometeam.reskiume.domain.model.rescueEvent
 
 import com.findmeahometeam.reskiume.data.database.entity.rescueEvent.NeedToCoverEntityForRecueEvent
 import com.findmeahometeam.reskiume.data.remote.response.rescueEvent.RemoteNeedToCoverForRescueEvent
+import org.jetbrains.compose.resources.StringResource
+import reskiume.composeapp.generated.resources.Res
+import reskiume.composeapp.generated.resources.need_to_cover_foster_home
+import reskiume.composeapp.generated.resources.need_to_cover_rescuers
+import reskiume.composeapp.generated.resources.need_to_cover_unselected
 
 data class NeedToCover(
     val needToCoverId: Long,
@@ -27,4 +32,12 @@ data class NeedToCover(
 
 enum class RescueNeed {
     UNSELECTED, RESCUERS, FOSTER_HOME
+}
+
+fun RescueNeed.toStringResource(): StringResource {
+    return when (this) {
+        RescueNeed.UNSELECTED -> Res.string.need_to_cover_unselected
+        RescueNeed.RESCUERS -> Res.string.need_to_cover_rescuers
+        RescueNeed.FOSTER_HOME -> Res.string.need_to_cover_foster_home
+    }
 }

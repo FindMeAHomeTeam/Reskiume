d15fe845d953840e4d8bbc1e309862992aaae4a8
juancoob <ja.cobos.obrero@gmail.com>
2026-03-17 12:00:46 +0100

    refactor: Create a custom saver for AcceptedNonHumanAnimalForFosterHome

--- composeApp/src/commonMain/kotlin/com/findmeahometeam/reskiume/domain/model/fosterHome/AcceptedNonHumanAnimalForFosterHome.kt
+++ composeApp/src/commonMain/kotlin/com/findmeahometeam/reskiume/domain/model/fosterHome/AcceptedNonHumanAnimalForFosterHome.kt
@@ -1,5 +1,7 @@
 package com.findmeahometeam.reskiume.domain.model.fosterHome
 
+import androidx.compose.runtime.saveable.Saver
+import androidx.compose.runtime.saveable.listSaver
 import com.findmeahometeam.reskiume.data.database.entity.fosterHome.AcceptedNonHumanAnimalEntityForFosterHome
 import com.findmeahometeam.reskiume.data.remote.response.fosterHome.RemoteAcceptedNonHumanAnimalForFosterHome
 import com.findmeahometeam.reskiume.domain.model.Gender
@@ -29,3 +31,22 @@ data class AcceptedNonHumanAnimalForFosterHome(
         )
     }
 }
+
+private fun AcceptedNonHumanAnimalForFosterHome.toSaveableList(): List<Any?> = listOf(
+    acceptedNonHumanAnimalId,
+    fosterHomeId,
+    acceptedNonHumanAnimalType,
+    acceptedNonHumanAnimalGender
+)
+
+private fun List<Any?>.fromSaveableList(): AcceptedNonHumanAnimalForFosterHome = AcceptedNonHumanAnimalForFosterHome(
+    acceptedNonHumanAnimalId = this[0] as Long,
+    fosterHomeId = this[1] as String,
+    acceptedNonHumanAnimalType = this[2] as NonHumanAnimalType,
+    acceptedNonHumanAnimalGender = this[3] as Gender
+)
+
+val AcceptedNonHumanAnimalForFosterHomeListSaver: Saver<List<AcceptedNonHumanAnimalForFosterHome>, Any> = listSaver(
+    save = { acceptedNonHumanAnimals -> acceptedNonHumanAnimals.map { it.toSaveableList() } },
+    restore = { savedList -> savedList.map { it.fromSaveableList() } }
+)

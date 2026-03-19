ec4f218438152c4419ef7026b77924850312f0c3
juancoob <ja.cobos.obrero@gmail.com>
2026-03-19 13:52:42 +0100

    test: Update FakeCheckNonHumanAnimalUtil to add more non human animals to check

--- composeApp/src/commonTest/kotlin/com/findmeahometeam/reskiume/ui/integrationTests/fakes/FakeCheckNonHumanAnimalUtil.kt
+++ composeApp/src/commonTest/kotlin/com/findmeahometeam/reskiume/ui/integrationTests/fakes/FakeCheckNonHumanAnimalUtil.kt
@@ -5,19 +5,15 @@ import com.findmeahometeam.reskiume.nonHumanAnimal
 import com.findmeahometeam.reskiume.ui.profile.checkNonHumanAnimal.CheckNonHumanAnimalUtil
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.flow.asFlow
 
 class FakeCheckNonHumanAnimalUtil(
-    private val nonHumanAnimalToReview: NonHumanAnimal = nonHumanAnimal
+    private val nonHumanAnimalToReviewList: List<NonHumanAnimal> = listOf(nonHumanAnimal)
 ): CheckNonHumanAnimalUtil {
 
     override fun getNonHumanAnimalFlow(
         nonHumanAnimalId: String,
         caregiverId: String,
         coroutineScope: CoroutineScope
-    ): Flow<NonHumanAnimal> = if(nonHumanAnimalToReview.id == nonHumanAnimalId && nonHumanAnimalToReview.caregiverId == caregiverId) {
-        flowOf(nonHumanAnimalToReview)
-    } else {
-        flowOf()
-    }
+    ): Flow<NonHumanAnimal> = nonHumanAnimalToReviewList.filter { it.id == nonHumanAnimalId && it.caregiverId == caregiverId }.asFlow()
 }

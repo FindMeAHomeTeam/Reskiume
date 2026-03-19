ef19d7a4b1d53a31808b514313242b750963903a
juancoob <ja.cobos.obrero@gmail.com>
2026-03-19 13:53:24 +0100

    test: Create FakeCheckAllMyRescueEventsUtil

---
+++ composeApp/src/commonTest/kotlin/com/findmeahometeam/reskiume/ui/integrationTests/fakes/FakeCheckAllMyRescueEventsUtil.kt
@@ -0,0 +1,21 @@
+package com.findmeahometeam.reskiume.ui.integrationTests.fakes
+
+import com.findmeahometeam.reskiume.domain.model.rescueEvent.RescueEvent
+import com.findmeahometeam.reskiume.ui.profile.checkAllMyRescueEvents.CheckAllMyRescueEventsUtil
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.flow.Flow
+
+class FakeCheckAllMyRescueEventsUtil: CheckAllMyRescueEventsUtil {
+
+    override fun downloadImageAndManageRescueEventsInLocalRepositoryFromFlow(
+        allRescueEventsFlow: Flow<List<RescueEvent>>,
+        myUid: String,
+        coroutineScope: CoroutineScope
+    ): Flow<List<RescueEvent>> = allRescueEventsFlow
+
+    override fun downloadImageAndModifyRescueEventsInLocalRepositoryFromFlow(
+        allRescueEventsFlow: Flow<List<RescueEvent>>,
+        myUid: String,
+        coroutineScope: CoroutineScope
+    ): Flow<List<RescueEvent>> = allRescueEventsFlow
+}

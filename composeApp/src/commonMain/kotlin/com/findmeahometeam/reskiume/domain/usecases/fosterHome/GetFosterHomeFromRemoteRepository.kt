90dd771d4962883133cead8b5c11aa5adb079d0d
juancoob <ja.cobos.obrero@gmail.com>
2026-03-28 14:53:26 +0100

    refactor: Update GetFosterHomeFromRemoteRepository to retrieve null in case the foster does not exist

--- composeApp/src/commonMain/kotlin/com/findmeahometeam/reskiume/domain/usecases/fosterHome/GetFosterHomeFromRemoteRepository.kt
+++ composeApp/src/commonMain/kotlin/com/findmeahometeam/reskiume/domain/usecases/fosterHome/GetFosterHomeFromRemoteRepository.kt
@@ -3,9 +3,9 @@ package com.findmeahometeam.reskiume.domain.usecases.fosterHome
 import com.findmeahometeam.reskiume.domain.model.fosterHome.FosterHome
 import com.findmeahometeam.reskiume.domain.repository.remote.fireStore.remoteFosterHome.FireStoreRemoteFosterHomeRepository
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.mapNotNull
+import kotlinx.coroutines.flow.map
 
 class GetFosterHomeFromRemoteRepository(private val fireStoreRemoteFosterHomeRepository: FireStoreRemoteFosterHomeRepository) {
-    operator fun invoke(id: String): Flow<FosterHome> =
-        fireStoreRemoteFosterHomeRepository.getRemoteFosterHome(id).mapNotNull { it?.toDomain() }
+    operator fun invoke(id: String): Flow<FosterHome?> =
+        fireStoreRemoteFosterHomeRepository.getRemoteFosterHome(id).map { it?.toDomain() }
 }

4b10248fd535570d248ebf21e3a570465e245284
juancoob <ja.cobos.obrero@gmail.com>
2026-03-30 13:20:55 +0200

    refactor: Update GetRescueEventFromRemoteRepository to retrieve nulls in case the rescue event does not exist

--- composeApp/src/commonMain/kotlin/com/findmeahometeam/reskiume/domain/usecases/rescueEvent/GetRescueEventFromRemoteRepository.kt
+++ composeApp/src/commonMain/kotlin/com/findmeahometeam/reskiume/domain/usecases/rescueEvent/GetRescueEventFromRemoteRepository.kt
@@ -3,9 +3,9 @@ package com.findmeahometeam.reskiume.domain.usecases.rescueEvent
 import com.findmeahometeam.reskiume.domain.model.rescueEvent.RescueEvent
 import com.findmeahometeam.reskiume.domain.repository.remote.fireStore.remoteRescueEvent.FireStoreRemoteRescueEventRepository
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.mapNotNull
+import kotlinx.coroutines.flow.map
 
 class GetRescueEventFromRemoteRepository(private val fireStoreRemoteRescueEventRepository: FireStoreRemoteRescueEventRepository) {
-    operator fun invoke(id: String): Flow<RescueEvent> =
-        fireStoreRemoteRescueEventRepository.getRemoteRescueEvent(id).mapNotNull { it?.toDomain() }
+    operator fun invoke(id: String): Flow<RescueEvent?> =
+        fireStoreRemoteRescueEventRepository.getRemoteRescueEvent(id).map { it?.toDomain() }
 }

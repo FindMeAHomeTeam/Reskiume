84ed7dd1a3343374b3d634d0766ea3a7eacffa72
juancoob <ja.cobos.obrero@gmail.com>
2026-04-08 17:04:21 +0200

    feat: Create the MessagingService for Android to get push notifications

---
+++ composeApp/src/androidMain/kotlin/com/findmeahometeam/reskiume/data/util/fcm/MessagingService.kt
@@ -0,0 +1,122 @@
+package com.findmeahometeam.reskiume.data.util.fcm
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Intent
+import android.media.RingtoneManager
+import android.os.Build
+import androidx.core.app.NotificationCompat
+import androidx.core.net.toUri
+import com.findmeahometeam.reskiume.MainActivity
+import com.findmeahometeam.reskiume.R
+import com.findmeahometeam.reskiume.data.util.log.Log
+import com.findmeahometeam.reskiume.domain.model.fosterHome.City
+import com.findmeahometeam.reskiume.domain.model.fosterHome.toStringResource
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+import org.koin.android.ext.android.inject
+
+class MessagingService() : FirebaseMessagingService() {
+
+    private val rescueEventsNotificationType = "rescueEvent"
+    private val rescueEventChannelId = "rescue_events"
+
+    private val messagingServiceViewModel: MessagingServiceViewModel by inject()
+    private val log: Log by inject()
+
+    override fun onMessageReceived(message: RemoteMessage) {
+        super.onMessageReceived(message)
+
+        messagingServiceViewModel.retrieveActivistId { activistId ->
+
+            if (activistId.isBlank()) {
+                log.d(
+                    "MessagingService",
+                    "No activist ID found, skipping notification"
+                )
+                return@retrieveActivistId
+            }
+
+            val notificationType = message.data["notificationType"] ?: ""
+            val creatorId = message.data["creatorId"] ?: ""
+
+            if (notificationType == rescueEventsNotificationType && creatorId != activistId) {
+
+                val cityStringResource = City.valueOf(message.data["city"]!!).toStringResource()
+
+                messagingServiceViewModel.retrieveStringResource(cityStringResource) { city ->
+
+                    val title =
+                        getString(R.string.notification_rescue_event_title, city.substring(5))
+                    val body = getString(R.string.notification_rescue_event_body)
+                    val deeplink = message.data["deeplink"] ?: ""
+
+                    sendNotification(
+                        title = title,
+                        body = body,
+                        deeplink = deeplink,
+                        channelId = rescueEventChannelId,
+                        channelName = getString(R.string.notification_rescue_event_channel)
+                    )
+                }
+            }
+        }
+    }
+
+    override fun onDeletedMessages() {
+        super.onDeletedMessages()
+        log.d("MessagingService", "onDeletedMessages called")
+    }
+
+    override fun onNewToken(token: String) {
+        super.onNewToken(token)
+        log.d("MessagingService", "onNewToken: $token")
+    }
+
+    private fun sendNotification(
+        title: String,
+        body: String,
+        deeplink: String,
+        channelId: String,
+        channelName: String
+    ) {
+        val requestCode = 0
+        val intent = if (deeplink.isBlank()) {
+            Intent(this, MainActivity::class.java)
+        } else {
+            Intent(Intent.ACTION_VIEW, deeplink.toUri())
+        }
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        val pendingIntent = PendingIntent.getActivity(
+            this,
+            requestCode,
+            intent,
+            PendingIntent.FLAG_IMMUTABLE or PendingIntent.FLAG_UPDATE_CURRENT,
+        )
+
+        val defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
+        val notificationBuilder = NotificationCompat.Builder(this, channelId)
+            .setSmallIcon(R.drawable.reskiume)
+            .setContentTitle(title)
+            .setContentText(body)
+            .setAutoCancel(true)
+            .setSound(defaultSoundUri)
+            .setContentIntent(pendingIntent)
+
+        val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+
+        // Since android Oreo notification channel is needed.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                channelId,
+                channelName,
+                NotificationManager.IMPORTANCE_DEFAULT,
+            )
+            notificationManager.createNotificationChannel(channel)
+        }
+
+        val notificationId = 0
+        notificationManager.notify(notificationId, notificationBuilder.build())
+    }
+}

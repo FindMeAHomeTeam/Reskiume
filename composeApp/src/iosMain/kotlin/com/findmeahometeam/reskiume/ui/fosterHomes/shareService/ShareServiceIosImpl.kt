c9270e32a5dda2ba3f4bebb54c3a751ea6183027
juancoob <ja.cobos.obrero@gmail.com>
2026-04-09 10:59:42 +0200

    refactor: Fix share service for ipads

--- composeApp/src/iosMain/kotlin/com/findmeahometeam/reskiume/ui/fosterHomes/shareService/ShareServiceIosImpl.kt
+++ composeApp/src/iosMain/kotlin/com/findmeahometeam/reskiume/ui/fosterHomes/shareService/ShareServiceIosImpl.kt
@@ -1,14 +1,20 @@
 package com.findmeahometeam.reskiume.ui.fosterHomes.shareService
 
 import kotlinx.cinterop.BetaInteropApi
+import kotlinx.cinterop.ExperimentalForeignApi
+import kotlinx.cinterop.useContents
+import platform.CoreGraphics.CGRectMake
 import platform.Foundation.NSString
 import platform.Foundation.create
 import platform.UIKit.UIActivityViewController
 import platform.UIKit.UIApplication
+import platform.UIKit.UIDevice
+import platform.UIKit.UIUserInterfaceIdiomPad
+import platform.UIKit.popoverPresentationController
 
-class ShareServiceIosImpl: ShareService  {
+class ShareServiceIosImpl : ShareService {
 
-    @OptIn(BetaInteropApi::class)
+    @OptIn(BetaInteropApi::class, ExperimentalForeignApi::class)
     override fun shareContent(
         text: String,
         onError: () -> Unit
@@ -23,6 +29,24 @@ class ShareServiceIosImpl: ShareService  {
         // Get the top-most view controller to present the activity view controller
         val rootViewController = UIApplication.sharedApplication.keyWindow?.rootViewController
         if (rootViewController != null) {
+
+            // Check if the device is an iPad
+            if (UIDevice.currentDevice.userInterfaceIdiom == UIUserInterfaceIdiomPad) {
+                val uiView = rootViewController.view
+                activityViewController.popoverPresentationController?.sourceView = uiView
+
+                // Point the popover to the center of the screen
+                uiView.bounds.useContents {
+                    activityViewController.popoverPresentationController?.sourceRect = CGRectMake(
+                        x = size.width / 2.0,
+                        y = size.height / 2.0,
+                        width = 0.0,
+                        height = 0.0
+                    )
+                }
+                // Disable the arrow so it just appears as a centered modal sheet
+                activityViewController.popoverPresentationController?.permittedArrowDirections = 0u
+            }
 
             rootViewController.presentViewController(
                 activityViewController,

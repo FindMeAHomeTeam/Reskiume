0eb00337bfb534b3f6bbaffa1fd5c99dd55d061d
juancoob <ja.cobos.obrero@gmail.com>
2026-04-11 12:32:02 +0200

    test: Update GetAllUsersFromLocalDataSourceTest with the new user changes

--- composeApp/src/commonTest/kotlin/com/findmeahometeam/reskiume/usecases/user/GetAllUsersFromLocalDataSourceTest.kt
+++ composeApp/src/commonTest/kotlin/com/findmeahometeam/reskiume/usecases/user/GetAllUsersFromLocalDataSourceTest.kt
@@ -1,19 +1,23 @@
 package com.findmeahometeam.reskiume.usecases.user
 
+import app.cash.turbine.test
+import com.findmeahometeam.reskiume.CoroutineTestDispatcher
 import com.findmeahometeam.reskiume.domain.repository.local.LocalUserRepository
 import com.findmeahometeam.reskiume.domain.usecases.user.GetAllUsersFromLocalDataSource
 import com.findmeahometeam.reskiume.user
+import com.findmeahometeam.reskiume.userWithAllSubscriptionData
 import dev.mokkery.answering.returns
 import dev.mokkery.everySuspend
 import dev.mokkery.mock
+import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.test.runTest
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
-class GetAllUsersFromLocalDataSourceTest {
+class GetAllUsersFromLocalDataSourceTest: CoroutineTestDispatcher() {
 
     val localUserRepository: LocalUserRepository = mock {
-        everySuspend { getAllUsers() } returns listOf(user.toEntity())
+        everySuspend { getAllUsers() } returns flowOf(listOf(userWithAllSubscriptionData))
     }
 
     private val getAllUsersFromLocalDataSource =
@@ -22,7 +26,10 @@ class GetAllUsersFromLocalDataSourceTest {
     @Test
     fun `given users_when the app request all the local users from the local database_then it retrieves them`() =
         runTest {
-            val actualUser = getAllUsersFromLocalDataSource()
-            assertEquals(listOf(user.copy(email = null)), actualUser)
+            getAllUsersFromLocalDataSource().test {
+
+                assertEquals(listOf(user.copy(email = null)), awaitItem())
+                awaitComplete()
+            }
         }
 }

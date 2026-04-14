3755f356b98341cfe417f72befd2e679bc27032f
juancoob <ja.cobos.obrero@gmail.com>
2026-04-14 18:51:12 +0200

    refactor: Update ContentView for iOS to handle push notifications in the foreground and the push notifications clicks

--- iosApp/iosApp/ContentView.swift
+++ iosApp/iosApp/ContentView.swift
@@ -21,9 +21,11 @@ struct ComposeView: UIViewControllerRepresentable {
 class AppDelegate: NSObject, UIApplicationDelegate, MessagingDelegate, UNUserNotificationCenterDelegate {
 
     func application(_ application: UIApplication,
-                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
+
         FirebaseApp.configure()
-        
+
+        UNUserNotificationCenter.current().delegate = self
         application.registerForRemoteNotifications()
         Messaging.messaging().delegate = self
         return true
@@ -37,6 +39,61 @@ class AppDelegate: NSObject, UIApplicationDelegate, MessagingDelegate, UNUserNot
     // Handle FCM token refresh
     func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
         print("Firebase registration token: \(String(describing: fcmToken))")
+    }
+
+    // Handle push notifications in the foreground
+    func userNotificationCenter(_ center: UNUserNotificationCenter,
+                                willPresent notification: UNNotification) async -> UNNotificationPresentationOptions {
+        let userInfo = notification.request.content.userInfo
+        print("Foreground notification: \(userInfo)")
+
+        // Check the data payload dictionary
+        let notificationType = userInfo["notificationType"] as? String ?? ""
+
+        if notificationType == "" {
+            print("notificationType is empty, skipping notification")
+            return []
+        }
+        let messagingServiceViewModel = MessagingServiceViewModel()
+        do {
+            var activistId = ""
+            try await messagingServiceViewModel.retrieveUserId { id in
+                
+                activistId = id
+            }
+            
+            if activistId == "" {
+                print("No activist ID found, skipping notification")
+                return []
+            }
+            
+            if notificationType == "rescueEvent" {
+                
+                let creatorId = userInfo["creatorId"] as? String
+                
+                if creatorId == activistId {
+                    print("Creator ID match with activist ID, skipping notification")
+                    return []
+                }
+            }
+            return [.list, .banner, .sound]
+        } catch {
+            print("Error retrieving user ID: \(error)")
+            return []
+        }
+    }
+
+    // Handle push notification clicks
+    func userNotificationCenter(_ center: UNUserNotificationCenter,
+                                didReceive response: UNNotificationResponse) async {
+        let userInfo = response.notification.request.content.userInfo
+        print("Notification clicked: \(userInfo)")
+
+        if let deeplink = userInfo["deeplink"] as? String,
+           let url = URL(string: deeplink) {
+
+            await UIApplication.shared.open(url)
+        }
     }
 }
 

9e17c63816987bca63def6380bdd62df868c8ca7
juancoob <ja.cobos.obrero@gmail.com>
2026-05-14 16:17:29 +0200

    refactor: Update GetAllMyChatsFromRemoteRepository to get the latest messages using the last one stored in the database

--- composeApp/src/commonMain/kotlin/com/findmeahometeam/reskiume/domain/usecases/chat/GetAllMyChatsFromRemoteRepository.kt
+++ composeApp/src/commonMain/kotlin/com/findmeahometeam/reskiume/domain/usecases/chat/GetAllMyChatsFromRemoteRepository.kt
@@ -1,31 +1,42 @@
 package com.findmeahometeam.reskiume.domain.usecases.chat
 
 import com.findmeahometeam.reskiume.domain.model.chat.Chat
+import com.findmeahometeam.reskiume.domain.repository.local.LocalChatRepository
 import com.findmeahometeam.reskiume.domain.repository.remote.fireStore.chat.FireStoreRemoteChatRepository
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.map
 
-class GetAllMyChatsFromRemoteRepository(private val fireStoreRemoteChatRepository: FireStoreRemoteChatRepository) {
-
+class GetAllMyChatsFromRemoteRepository(
+    private val fireStoreRemoteChatRepository: FireStoreRemoteChatRepository,
+    private val localChatRepository: LocalChatRepository
+) {
     operator fun invoke(
         uid: String,
-        lastTimestamp: Long = 0L
+        lastChatTimestamp: Long = 0L
     ): Flow<List<Chat>> =
-        fireStoreRemoteChatRepository.getAllMyRemoteChats(uid)
-            .map { list ->
+        fireStoreRemoteChatRepository.getAllMyRemoteChats(
+            uid,
+            lastChatTimestamp
+        ).map { list ->
+            list.map { remoteChat ->
 
-                list.map { remoteChat ->
+                val allRemoteChatMessages = fireStoreRemoteChatRepository.getRemoteChatMessages(
+                    chatId = remoteChat.id!!,
+                    lastTimestamp = localChatRepository.getAllMyChatMessages(remoteChat.id).first()
+                        .let {
+                            if (it.isEmpty()) {
+                                0L
+                            } else {
+                                it.last().timestamp
+                            }
+                        }
+                ).first()
 
-                    val allChatMessages = fireStoreRemoteChatRepository.getRemoteChatMessages(
-                        chatId = remoteChat.id!!,
-                        lastTimestamp = lastTimestamp
-                    ).first()
-
-                    remoteChat.toDomain(
-                        myUid = uid,
-                        allChatMessages = allChatMessages
-                    )
-                }
+                remoteChat.toDomain(
+                    myUid = uid,
+                    allChatMessages = allRemoteChatMessages
+                )
             }
+        }
 }

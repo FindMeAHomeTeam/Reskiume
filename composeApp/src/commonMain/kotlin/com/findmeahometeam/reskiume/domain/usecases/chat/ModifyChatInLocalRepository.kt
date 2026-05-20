33c95e574c2f04b9228210f62e60070d2d8a9946
juancoob <ja.cobos.obrero@gmail.com>
2026-05-20 22:56:59 +0200

    refactor: Update ModifyChatInLocalRepository to modify all chat models before the chat itself to avoid outdated updates

--- composeApp/src/commonMain/kotlin/com/findmeahometeam/reskiume/domain/usecases/chat/ModifyChatInLocalRepository.kt
+++ composeApp/src/commonMain/kotlin/com/findmeahometeam/reskiume/domain/usecases/chat/ModifyChatInLocalRepository.kt
@@ -15,37 +15,35 @@ class ModifyChatInLocalRepository(
         previousChat: Chat,
         onModifyChat: suspend (isUpdated: Boolean) -> Unit
     ) {
-        localChatRepository.modifyChat(
-            updatedChat.toEntity(),
-            onModifyChat = { rowsUpdated ->
-                if (rowsUpdated > 0) {
-                    var isSuccess =
-                        manageAllNonHumanAnimalsInfo(
-                            previousChat,
-                            updatedChat
-                        )
+        manageAllNonHumanAnimalsInfo(
+            previousChat,
+            updatedChat
+        ) {
+            manageAllActivistsInfo(
+                previousChat,
+                updatedChat
+            ) {
+                manageAllBlockedUsersInfo(
+                    previousChat,
+                    updatedChat
+                ) {
+                    localChatRepository.modifyChat(
+                        updatedChat.toEntity(),
+                        onModifyChat = { rowsUpdated ->
 
-                    if (isSuccess) {
-                        isSuccess = manageAllActivistsInfo(previousChat, updatedChat)
-
-                        if (isSuccess) {
-                            isSuccess = manageAllBlockedUsersInfo(previousChat, updatedChat)
+                            onModifyChat(rowsUpdated > 0)
                         }
-                    }
-                    onModifyChat(isSuccess)
-                } else {
-                    onModifyChat(false)
+                    )
                 }
             }
-        )
+        }
     }
-    
+
     private suspend fun manageAllNonHumanAnimalsInfo(
         previousChat: Chat,
-        updatedChat: Chat
-    ): Boolean {
-        var isSuccess = true
-
+        updatedChat: Chat,
+        onComplete: suspend () -> Unit
+    ) {
         val previousAllNonHumanAnimalsInfo =
             previousChat.allNonHumanAnimalsInfo.toSet()
 
@@ -56,69 +54,68 @@ class ModifyChatInLocalRepository(
             (previousAllNonHumanAnimalsInfo - updatedAllNonHumanAnimalsInfo) +
                     (updatedAllNonHumanAnimalsInfo - previousAllNonHumanAnimalsInfo)
 
+        if (nonHumanAnimalsInfoToManage.isEmpty()) {
+            onComplete()
+            return
+        }
         nonHumanAnimalsInfoToManage.forEach { nonHumanAnimalInfoToManage ->
-            if (isSuccess) {
-                
-                if (updatedAllNonHumanAnimalsInfo.contains(nonHumanAnimalInfoToManage)) {
-                    
-                    val nonHumanAnimalInfoEntity =
-                        updatedChat.allNonHumanAnimalsInfo.first {
-                            it.nonHumanAnimalId == nonHumanAnimalInfoToManage.nonHumanAnimalId
-                        }.toEntity()
 
-                    localChatRepository.insertNonHumanAnimalInfoEntity(
-                        nonHumanAnimalInfoEntity,
-                        onInsertNonHumanAnimalInfoEntity = { rowId ->
-                            if (rowId > 0) {
-                                log.d(
-                                    "ModifyChatInLocalRepository",
-                                    "manageAllNonHumanAnimalsInfo: inserted the non human animal info ${nonHumanAnimalInfoEntity.nonHumanAnimalId} in the chat ${nonHumanAnimalInfoEntity.chatId} in the local data source"
-                                )
-                            } else {
-                                log.e(
-                                    "ModifyChatInLocalRepository",
-                                    "manageAllNonHumanAnimalsInfo: failed to insert the non human animal info ${nonHumanAnimalInfoEntity.nonHumanAnimalId} in the chat ${nonHumanAnimalInfoEntity.chatId} in the local data source"
-                                )
-                                isSuccess = false
-                            }
-                        }
-                    )
-                } else {
+            if (updatedAllNonHumanAnimalsInfo.contains(nonHumanAnimalInfoToManage)) {
 
-                    val nonHumanAnimalInfo =
-                        previousChat.allNonHumanAnimalsInfo.first {
-                            it.nonHumanAnimalId == nonHumanAnimalInfoToManage.nonHumanAnimalId
-                        }
+                val nonHumanAnimalInfoEntity =
+                    updatedChat.allNonHumanAnimalsInfo.first {
+                        it.nonHumanAnimalId == nonHumanAnimalInfoToManage.nonHumanAnimalId
+                    }.toEntity()
 
-                    localChatRepository.deleteNonHumanAnimalInfoEntity(
-                        nonHumanAnimalInfo.nonHumanAnimalId,
-                        onDeleteNonHumanAnimalInfoEntity = { rowsDeleted ->
-                            if (rowsDeleted > 0) {
-                                log.d(
-                                    "ModifyChatInLocalRepository",
-                                    "manageAllNonHumanAnimalsInfo: deleted the non human animal info ${nonHumanAnimalInfo.nonHumanAnimalId} in the chat ${nonHumanAnimalInfo.chatId} in the local data source"
-                                )
-                            } else {
-                                log.e(
-                                    "ModifyChatInLocalRepository",
-                                    "manageAllNonHumanAnimalsInfo: failed to delete the non human animal info ${nonHumanAnimalInfo.nonHumanAnimalId} in the chat ${nonHumanAnimalInfo.chatId} in the local data source"
-                                )
-                                isSuccess = false
-                            }
+                localChatRepository.insertNonHumanAnimalInfoEntity(
+                    nonHumanAnimalInfoEntity,
+                    onInsertNonHumanAnimalInfoEntity = { rowId ->
+                        if (rowId > 0) {
+                            log.d(
+                                "ModifyChatInLocalRepository",
+                                "manageAllNonHumanAnimalsInfo: inserted the non human animal info ${nonHumanAnimalInfoEntity.nonHumanAnimalId} in the chat ${nonHumanAnimalInfoEntity.chatId} in the local data source"
+                            )
+                        } else {
+                            log.e(
+                                "ModifyChatInLocalRepository",
+                                "manageAllNonHumanAnimalsInfo: failed to insert the non human animal info ${nonHumanAnimalInfoEntity.nonHumanAnimalId} in the chat ${nonHumanAnimalInfoEntity.chatId} in the local data source"
+                            )
                         }
-                    )
-                }
+                    }
+                )
+            } else {
+
+                val nonHumanAnimalInfo =
+                    previousChat.allNonHumanAnimalsInfo.first {
+                        it.nonHumanAnimalId == nonHumanAnimalInfoToManage.nonHumanAnimalId
+                    }
+
+                localChatRepository.deleteNonHumanAnimalInfoEntity(
+                    nonHumanAnimalInfo.nonHumanAnimalId,
+                    onDeleteNonHumanAnimalInfoEntity = { rowsDeleted ->
+                        if (rowsDeleted > 0) {
+                            log.d(
+                                "ModifyChatInLocalRepository",
+                                "manageAllNonHumanAnimalsInfo: deleted the non human animal info ${nonHumanAnimalInfo.nonHumanAnimalId} in the chat ${nonHumanAnimalInfo.chatId} in the local data source"
+                            )
+                        } else {
+                            log.e(
+                                "ModifyChatInLocalRepository",
+                                "manageAllNonHumanAnimalsInfo: failed to delete the non human animal info ${nonHumanAnimalInfo.nonHumanAnimalId} in the chat ${nonHumanAnimalInfo.chatId} in the local data source"
+                            )
+                        }
+                    }
+                )
             }
         }
-        return isSuccess
+        onComplete()
     }
 
     private suspend fun manageAllActivistsInfo(
         previousChat: Chat,
-        updatedChat: Chat
-    ): Boolean {
-        var isSuccess = true
-
+        updatedChat: Chat,
+        onComplete: suspend () -> Unit
+    ) {
         val previousAllActivistsInfo =
             previousChat.allActivistsInfo.map { it.uid }.toSet()
 
@@ -129,68 +126,67 @@ class ModifyChatInLocalRepository(
             (previousAllActivistsInfo - updatedAllActivistsInfo) +
                     (updatedAllActivistsInfo - previousAllActivistsInfo)
 
+        if (activistUidsToManage.isEmpty()) {
+            onComplete()
+            return
+        }
         activistUidsToManage.forEach { uidToManage ->
-            if (isSuccess) {
 
-                if (updatedAllActivistsInfo.contains(uidToManage)) {
+            if (updatedAllActivistsInfo.contains(uidToManage)) {
 
-                    val userInfoEntity =
-                        updatedChat.allActivistsInfo.first {
-                            it.uid == uidToManage
-                        }.toEntity()
+                val userInfoEntity =
+                    updatedChat.allActivistsInfo.first {
+                        it.uid == uidToManage
+                    }.toEntity()
 
-                    localChatRepository.insertActivistInfoEntity(
-                        userInfoEntity,
-                        onInsertActivistInfoEntity = { rowId ->
-                            if (rowId > 0) {
-                                log.d(
-                                    "ModifyChatInLocalRepository",
-                                    "manageAllActivistsInfo: inserted the activist ${userInfoEntity.uid} in the chat ${userInfoEntity.chatId} in the local data source"
-                                )
-                            } else {
-                                log.e(
-                                    "ModifyChatInLocalRepository",
-                                    "manageAllActivistsInfo: failed to insert the activist ${userInfoEntity.uid} in the chat ${userInfoEntity.chatId} in the local data source"
-                                )
-                                isSuccess = false
-                            }
+                localChatRepository.insertActivistInfoEntity(
+                    userInfoEntity,
+                    onInsertActivistInfoEntity = { rowId ->
+                        if (rowId > 0) {
+                            log.d(
+                                "ModifyChatInLocalRepository",
+                                "manageAllActivistsInfo: inserted the activist ${userInfoEntity.uid} in the chat ${userInfoEntity.chatId} in the local data source"
+                            )
+                        } else {
+                            log.e(
+                                "ModifyChatInLocalRepository",
+                                "manageAllActivistsInfo: failed to insert the activist ${userInfoEntity.uid} in the chat ${userInfoEntity.chatId} in the local data source"
+                            )
                         }
-                    )
-                } else {
+                    }
+                )
+            } else {
 
-                    val userInfo =
-                        previousChat.allActivistsInfo.first {
-                            it.uid == uidToManage
+                val userInfo =
+                    previousChat.allActivistsInfo.first {
+                        it.uid == uidToManage
+                    }
+                localChatRepository.deleteActivistInfoEntity(
+                    userInfo.uid,
+                    onDeleteActivistInfoEntity = { rowsDeleted ->
+                        if (rowsDeleted > 0) {
+                            log.d(
+                                "ModifyChatInLocalRepository",
+                                "manageAllActivistsInfo: deleted the activist ${userInfo.uid} in the chat ${userInfo.chatId} in the local data source"
+                            )
+                        } else {
+                            log.e(
+                                "ModifyChatInLocalRepository",
+                                "manageAllActivistsInfo: failed to delete the activist ${userInfo.uid} in the chat ${userInfo.chatId} in the local data source"
+                            )
                         }
-                    localChatRepository.deleteActivistInfoEntity(
-                        userInfo.uid,
-                        onDeleteActivistInfoEntity = { rowsDeleted ->
-                            if (rowsDeleted > 0) {
-                                log.d(
-                                    "ModifyChatInLocalRepository",
-                                    "manageAllActivistsInfo: deleted the activist ${userInfo.uid} in the chat ${userInfo.chatId} in the local data source"
-                                )
-                            } else {
-                                log.e(
-                                    "ModifyChatInLocalRepository",
-                                    "manageAllActivistsInfo: failed to delete the activist ${userInfo.uid} in the chat ${userInfo.chatId} in the local data source"
-                                )
-                                isSuccess = false
-                            }
-                        }
-                    )
-                }
+                    }
+                )
             }
         }
-        return isSuccess
+        onComplete()
     }
 
     private suspend fun manageAllBlockedUsersInfo(
         previousChat: Chat,
-        updatedChat: Chat
-    ): Boolean {
-        var isSuccess = true
-
+        updatedChat: Chat,
+        onComplete: suspend () -> Unit
+    ) {
         val previousAllBlockedUsersInfo =
             previousChat.allBlockedUsersInfo.toSet()
 
@@ -201,60 +197,60 @@ class ModifyChatInLocalRepository(
             (previousAllBlockedUsersInfo - updatedAllBlockedUsersInfo) +
                     (updatedAllBlockedUsersInfo - previousAllBlockedUsersInfo)
 
+        if (blockedUsersInfoToManage.isEmpty()) {
+            onComplete()
+            return
+        }
         blockedUsersInfoToManage.forEach { userInfoToManage ->
-            if (isSuccess) {
 
-                if (updatedAllBlockedUsersInfo.contains(userInfoToManage)) {
+            if (updatedAllBlockedUsersInfo.contains(userInfoToManage)) {
 
-                    val blockedUserInfoEntity =
-                        updatedChat.allBlockedUsersInfo.first {
-                            it.uid == userInfoToManage.uid
-                        }.toEntity()
+                val blockedUserInfoEntity =
+                    updatedChat.allBlockedUsersInfo.first {
+                        it.uid == userInfoToManage.uid
+                    }.toEntity()
 
-                    localChatRepository.insertBlockedUserInfoEntity(
-                        blockedUserInfoEntity,
-                        onInsertBlockedUserInfoEntity = { rowId ->
-                            if (rowId > 0) {
-                                log.d(
-                                    "ModifyChatInLocalRepository",
-                                    "manageAllBlockedUsersInfo: inserted the blocked user ${blockedUserInfoEntity.uid} in the chat ${blockedUserInfoEntity.chatId} in the local data source"
-                                )
-                            } else {
-                                log.e(
-                                    "ModifyChatInLocalRepository",
-                                    "manageAllBlockedUsersInfo: failed to insert the blocked user ${blockedUserInfoEntity.uid} in the chat ${blockedUserInfoEntity.chatId} in the local data source"
-                                )
-                                isSuccess = false
-                            }
+                localChatRepository.insertBlockedUserInfoEntity(
+                    blockedUserInfoEntity,
+                    onInsertBlockedUserInfoEntity = { rowId ->
+                        if (rowId > 0) {
+                            log.d(
+                                "ModifyChatInLocalRepository",
+                                "manageAllBlockedUsersInfo: inserted the blocked user ${blockedUserInfoEntity.uid} in the chat ${blockedUserInfoEntity.chatId} in the local data source"
+                            )
+                        } else {
+                            log.e(
+                                "ModifyChatInLocalRepository",
+                                "manageAllBlockedUsersInfo: failed to insert the blocked user ${blockedUserInfoEntity.uid} in the chat ${blockedUserInfoEntity.chatId} in the local data source"
+                            )
                         }
-                    )
-                } else {
+                    }
+                )
+            } else {
 
-                    val blockedUserInfo =
-                        previousChat.allBlockedUsersInfo.first {
-                            it.uid == userInfoToManage.uid
-                        }
+                val blockedUserInfo =
+                    previousChat.allBlockedUsersInfo.first {
+                        it.uid == userInfoToManage.uid
+                    }
 
-                    localChatRepository.deleteBlockedUserInfoEntity(
-                        blockedUserInfo.uid,
-                        onDeleteBlockedUserInfoEntity = { rowsDeleted ->
-                            if (rowsDeleted > 0) {
-                                log.d(
-                                    "ModifyChatInLocalRepository",
-                                    "manageAllBlockedUsersInfo: deleted the blocked user ${blockedUserInfo.uid} in the chat ${blockedUserInfo.chatId} in the local data source"
-                                )
-                            } else {
-                                log.e(
-                                    "ModifyChatInLocalRepository",
-                                    "manageAllBlockedUsersInfo: failed to delete the blocked user ${blockedUserInfo.uid} in the chat ${blockedUserInfo.chatId} in the local data source"
-                                )
-                                isSuccess = false
-                            }
+                localChatRepository.deleteBlockedUserInfoEntity(
+                    blockedUserInfo.uid,
+                    onDeleteBlockedUserInfoEntity = { rowsDeleted ->
+                        if (rowsDeleted > 0) {
+                            log.d(
+                                "ModifyChatInLocalRepository",
+                                "manageAllBlockedUsersInfo: deleted the blocked user ${blockedUserInfo.uid} in the chat ${blockedUserInfo.chatId} in the local data source"
+                            )
+                        } else {
+                            log.e(
+                                "ModifyChatInLocalRepository",
+                                "manageAllBlockedUsersInfo: failed to delete the blocked user ${blockedUserInfo.uid} in the chat ${blockedUserInfo.chatId} in the local data source"
+                            )
                         }
-                    )
-                }
+                    }
+                )
             }
         }
-        return isSuccess
+        onComplete()
     }
 }

c872d2f55e16a5a7686d73a18dc76b92858e6319
juancoob <ja.cobos.obrero@gmail.com>
2026-05-22 23:10:36 +0200

    test: Update FakeSaveStateHandleProvider objectRoute parameter

--- composeApp/src/commonTest/kotlin/com/findmeahometeam/reskiume/ui/integrationTests/fakes/FakeSaveStateHandleProvider.kt
+++ composeApp/src/commonTest/kotlin/com/findmeahometeam/reskiume/ui/integrationTests/fakes/FakeSaveStateHandleProvider.kt
@@ -1,7 +1,7 @@
 package com.findmeahometeam.reskiume.ui.integrationTests.fakes
 
 import androidx.navigation.NavType
-import com.findmeahometeam.reskiume.ui.core.navigation.CheckReviews
+import com.findmeahometeam.reskiume.ui.core.navigation.CheckAllReviews
 import com.findmeahometeam.reskiume.ui.core.navigation.SaveStateHandleProvider
 import com.findmeahometeam.reskiume.user
 import kotlin.jvm.JvmSuppressWildcards
@@ -10,7 +10,7 @@ import kotlin.reflect.KType
 
 @Suppress("UNCHECKED_CAST")
 class FakeSaveStateHandleProvider(
-    private val objectRoute: Any = CheckReviews(user.uid)
+    private val objectRoute: Any = CheckAllReviews(user.uid)
 ): SaveStateHandleProvider {
     override fun <T : Any> provideObjectRoute(
         route: KClass<T>,

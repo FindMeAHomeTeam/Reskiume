92d5930d26b1bff9027ceb23e41534f7cbc2e58c
juancoob <ja.cobos.obrero@gmail.com>
2026-05-22 22:56:03 +0200

    refactor: Update CheckAllReviewsViewmodel to get reviews with UiState and get the user data as an state flow to avoid requesting it every time

--- composeApp/src/commonMain/kotlin/com/findmeahometeam/reskiume/ui/profile/checkReviews/CheckAllReviewsViewmodel.kt
+++ composeApp/src/commonMain/kotlin/com/findmeahometeam/reskiume/ui/profile/checkReviews/CheckAllReviewsViewmodel.kt
@@ -1,29 +1,39 @@
 package com.findmeahometeam.reskiume.ui.profile.checkReviews
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.findmeahometeam.reskiume.data.remote.response.AuthUser
 import com.findmeahometeam.reskiume.domain.model.user.User
 import com.findmeahometeam.reskiume.domain.usecases.authUser.ObserveAuthStateInAuthDataSource
-import com.findmeahometeam.reskiume.ui.core.navigation.CheckReviews
+import com.findmeahometeam.reskiume.ui.core.components.UiState
+import com.findmeahometeam.reskiume.ui.core.components.toUiState
+import com.findmeahometeam.reskiume.ui.core.navigation.CheckAllReviews
 import com.findmeahometeam.reskiume.ui.core.navigation.SaveStateHandleProvider
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.flatMapConcat
 import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.flow.stateIn
 
-class CheckReviewsViewmodel(
+class CheckAllReviewsViewmodel(
     saveStateHandleProvider: SaveStateHandleProvider,
     checkReviewsUtil: CheckReviewsUtil,
-    private val observeAuthStateInAuthDataSource: ObserveAuthStateInAuthDataSource,
+    observeAuthStateInAuthDataSource: ObserveAuthStateInAuthDataSource,
     private val checkActivistUtil: CheckActivistUtil
 ) : ViewModel() {
 
-    private val reviewedUid = saveStateHandleProvider.provideObjectRoute(CheckReviews::class).uid
+    private val reviewedUid = saveStateHandleProvider.provideObjectRoute(CheckAllReviews::class).uid
 
-    val reviewListFlow: Flow<List<UiReview>> = checkReviewsUtil.getReviewListFlow(reviewedUid)
+    val reviewListState: StateFlow<UiState<List<UiReview>>> =
+        checkReviewsUtil.getReviewListFlow(reviewedUid).toUiState().stateIn(
+            viewModelScope,
+            SharingStarted.WhileSubscribed(5_000),
+            UiState.Loading()
+        )
 
     @OptIn(ExperimentalCoroutinesApi::class)
-    fun getUserDataIfNotMine(): Flow<User?> =
+    val userDataIfNotMine: StateFlow<User?> =
         observeAuthStateInAuthDataSource().flatMapConcat { authUser: AuthUser? ->
 
             if (authUser?.uid == reviewedUid) {
@@ -31,7 +41,11 @@ class CheckReviewsViewmodel(
             } else {
                 flowOf(getActivist(authUser?.uid ?: " "))
             }
-        }
+        }.stateIn(
+            viewModelScope,
+            SharingStarted.WhileSubscribed(5_000),
+            null
+        )
 
     private suspend fun getActivist(myUserUid: String): User? {
 
